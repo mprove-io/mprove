a44db2f5ca864321b802dc939085fefdb5d742c7
akalitenya <akalitenya@mprove.io>
2018-11-12 17:27:47 +0500

    statis assets url

--- src/app/configs/path.config.ts
+++ src/app/configs/path.config.ts
@@ -1,12 +1,11 @@
 import { environment } from '@env/environment';
 
-// const host = window.location.host;
-// const protocol = window.location.protocol;
+const host = window.location.host;
+const protocol = window.location.protocol;
 // const wsProtocol = protocol === 'https:' ? 'wss:' : 'ws:';
 
 export let pathConfig = {
-  // staticAssetsBaseUrl: `${protocol}//${host}`,
-  staticAssetsBaseUrl: environment.staticAssetsBaseUrl,
+  staticAssetsBaseUrl: `${protocol}//${host}`,
   dynamicAssetsBaseUrl: environment.dynamicAssetsBaseUrl,
   httpUrl: environment.httpUrl,
   websocketUrl: environment.websocketUrl,
--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -13,7 +13,6 @@ export const environment = {
   canUseStoreFreeze: false,
   canUseSegmentMetaReducer: false,
 
-  staticAssetsBaseUrl: 'http://localhost:8088',
   dynamicAssetsBaseUrl: 'http://localhost:8080',
   httpUrl: 'http://localhost:8080/api/v1',
   websocketUrl: 'ws://localhost:8080/api/v1/webchat/',
