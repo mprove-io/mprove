8fc1b6c6fd1938f146dd5f1fa441594d5b0d7f30
akalitenya <akalitenya@mprove.io>
2018-11-15 19:05:23 +0500

    api

--- src/api/api-client-to-server/repos/regenerate-repo-remote-public-key/regenerate-repo-remote-public-key-request-body.ts
+++ src/api/api-client-to-server/repos/regenerate-repo-remote-public-key/regenerate-repo-remote-public-key-request-body.ts
@@ -1,4 +1,5 @@
 import * as apiObjects from '../../../objects/_index';
+// tslint:disable-next-line:max-line-length
 import { RegenerateRepoRemotePublicKeyRequestBodyPayload } from './regenerate-repo-remote-public-key-request-body-payload';
 
 export interface RegenerateRepoRemotePublicKeyRequestBody {
--- src/api/api-client-to-server/repos/regenerate-repo-remote-webhook/regenerate-repo-remote-webhook-response-200-body.ts
+++ src/api/api-client-to-server/repos/regenerate-repo-remote-webhook/regenerate-repo-remote-webhook-response-200-body.ts
@@ -1,4 +1,5 @@
 import * as apiObjects from '../../../objects/_index';
+// tslint:disable-next-line:max-line-length
 import { RegenerateRepoRemoteWebhookResponse200BodyPayload } from './regenerate-repo-remote-webhook-response-200-body-payload';
 
 export interface RegenerateRepoRemoteWebhookResponse200Body {
