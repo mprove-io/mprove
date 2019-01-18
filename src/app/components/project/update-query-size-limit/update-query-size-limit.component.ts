23f5113e87719879894eaeedd85efe7ca696c16d
akalitenya <akalitenya@mprove.io>
2019-01-18 16:29:59 +0500

    fix edit project query data size limit

--- src/app/components/project/update-query-size-limit/update-query-size-limit.component.ts
+++ src/app/components/project/update-query-size-limit/update-query-size-limit.component.ts
@@ -1,19 +1,47 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import * as actions from 'app/store/actions/_index';
 import * as api from 'app/api/_index';
 import * as interfaces from 'app/interfaces/_index';
 import * as selectors from 'app/store/selectors/_index';
+import { tap } from 'rxjs/operators';
 
 @Component({
   moduleId: module.id,
   selector: 'm-update-query-size-limit',
   templateUrl: 'update-query-size-limit.component.html'
 })
-export class UpdateQuerySizeLimitComponent implements OnChanges {
-  @Input()
-  selectedProject: api.Project;
+export class UpdateQuerySizeLimitComponent implements OnInit {
+  querySizeLimit: number;
+  querySizeLimit$ = this.store
+    .select(selectors.getSelectedProjectQuerySizeLimit)
+    .pipe(
+      tap(x => {
+        this.querySizeLimit = x;
+        this.updateQuerySizeLimitForm.patchValue({
+          querySizeLimit: this.querySizeLimit
+        });
+        this.checkForm();
+      })
+    );
+
+  selectedProjectServerTs: number;
+  selectedProjectServerTs$ = this.store
+    .select(selectors.getSelectedProjectServerTs)
+    .pipe(
+      tap(x => {
+        this.selectedProjectServerTs = x;
+      })
+    );
+
+  selectedProjectId: string;
+  selectedProjectId$ = this.store.select(selectors.getSelectedProjectId).pipe(
+    tap(x => {
+      this.selectedProjectId = x;
+    })
+  );
+
   updateQuerySizeLimitForm: FormGroup = null;
 
   selectedProjectUserIsAdmin$ = this.store.select(
@@ -25,21 +53,15 @@ export class UpdateQuerySizeLimitComponent implements OnChanges {
     private store: Store<interfaces.AppState>
   ) {}
 
-  ngOnChanges(): void {
-    if (!this.updateQuerySizeLimitForm) {
-      this.buildForm();
-    } else if (
-      this.updateQuerySizeLimitForm.get('querySizeLimit').value ===
-      this.selectedProject.query_size_limit
-    ) {
-      this.updateQuerySizeLimitForm.markAsPristine();
-    }
+  ngOnInit(): void {
+    this.buildForm();
+    this.checkForm();
   }
 
   buildForm(): void {
     this.updateQuerySizeLimitForm = this.fb.group({
       querySizeLimit: [
-        this.selectedProject.query_size_limit,
+        this.querySizeLimit,
         Validators.compose([
           Validators.required,
           Validators.min(0),
@@ -49,19 +71,35 @@ export class UpdateQuerySizeLimitComponent implements OnChanges {
     });
   }
 
+  checkForm() {
+    if (
+      this.updateQuerySizeLimitForm.get('querySizeLimit').value ===
+      this.querySizeLimit
+    ) {
+      this.updateQuerySizeLimitForm.markAsPristine();
+    }
+  }
+
   onSubmit(form: FormGroup, fv: any) {
-    this.store.dispatch(
-      new actions.SetProjectQuerySizeLimitAction({
-        project_id: this.selectedProject.project_id,
-        server_ts: this.selectedProject.server_ts,
-        query_size_limit: fv['querySizeLimit']
-      })
-    );
+    if (
+      this.updateQuerySizeLimitForm.get('querySizeLimit').value !==
+      this.querySizeLimit
+    ) {
+      this.store.dispatch(
+        new actions.SetProjectQuerySizeLimitAction({
+          project_id: this.selectedProjectId,
+          server_ts: this.selectedProjectServerTs,
+          query_size_limit: fv['querySizeLimit']
+        })
+      );
+    } else {
+      this.updateQuerySizeLimitForm.markAsPristine();
+    }
   }
 
   onReset(form: FormGroup) {
     form.patchValue({
-      querySizeLimit: this.selectedProject.query_size_limit
+      querySizeLimit: this.querySizeLimit
     });
     form.markAsPristine();
   }
--- src/app/store/selectors/get-selected-project/_index.ts
+++ src/app/store/selectors/get-selected-project/_index.ts
@@ -11,6 +11,7 @@ export * from './get-selected-project-client-email';
 export * from './get-selected-project-id';
 export * from './get-selected-project-is-special';
 export * from './get-selected-project-query-size-limit';
+export * from './get-selected-project-server-ts';
 export * from './get-selected-project-themes-restricted';
 export * from './get-selected-project-timezone';
 export * from './get-selected-project-week-start';
