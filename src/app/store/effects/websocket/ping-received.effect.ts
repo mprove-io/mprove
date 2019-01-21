2f43bbcd74310ec588dff1fbca72b2ab478e2b6d
akalitenya <akalitenya@mprove.io>
2019-01-21 17:27:59 +0500

    pong init_id

--- src/app/store/effects/websocket/ping-received.effect.ts
+++ src/app/store/effects/websocket/ping-received.effect.ts
@@ -1,21 +1,34 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { Action } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { mergeMap } from 'rxjs/operators';
+import { Action, Store } from '@ngrx/store';
+import { Observable, from } from 'rxjs';
+import { mergeMap, take } from 'rxjs/operators';
 import * as actions from 'app/store/actions/_index';
+import * as selectors from 'app/store/selectors/_index';
+import * as interfaces from 'app/interfaces/_index';
 import * as actionTypes from 'app/store/action-types';
 
 @Injectable()
 export class PingReceivedEffect {
   @Effect() pingReceived$: Observable<Action> = this.actions$.pipe(
     ofType(actionTypes.PING_RECEIVED),
-    mergeMap((action: actions.PingReceivedAction) => [
-      // TODO: #23-2 check need of from()
-      new actions.PongAction({ reply_to: action.payload.info.request_id }),
-      new actions.UpdateLayoutLastWebsocketMessageTimestampAction(Date.now())
-    ])
+    mergeMap((action: actions.PingReceivedAction) => {
+      let initId;
+
+      this.store
+        .select(selectors.getWebSocketInitId)
+        .pipe(take(1))
+        .subscribe(x => (initId = x));
+
+      return from([
+        new actions.PongAction({ init_id: initId }),
+        new actions.UpdateLayoutLastWebsocketMessageTimestampAction(Date.now())
+      ]);
+    })
   );
 
-  constructor(private actions$: Actions) {}
+  constructor(
+    private actions$: Actions,
+    private store: Store<interfaces.AppState>
+  ) {}
 }
