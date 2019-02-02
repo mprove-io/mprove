ec0695c48748b6347baa237e19121369ccf36ec2
akalitenya <akalitenya@mprove.io>
2019-02-02 11:27:53 +0500

    cypress logout

---
+++ cypress/integration/7-profile/7-7-profile-data.spec.ts
@@ -0,0 +1,27 @@
+import * as constants from '../../../src/app/constants/_index';
+import * as api from '../../../src/app/api/_index';
+
+const userId = '7-7-user@example.com';
+const password = '123123';
+
+describe('7-7 profile-data (logged in)', () => {
+  it(`should see data`, () => {
+    cy.deletePack({ user_ids: [userId] });
+    cy.seedPack({
+      users: [
+        {
+          user_id: userId,
+          password: password,
+          email_verified: true
+        }
+      ]
+    });
+    cy.loginUser({ user_id: userId, password: password });
+    cy.basicVisit(constants.PATH_PROFILE);
+    cy.loading();
+    cy.get('[data-cy=editNameFirstNameInput]').should('have.value', '');
+    cy.get('[data-cy=editNameLastNameInput]').should('have.value', '');    
+    cy.get('[data-cy=updateUserTimezoneSelect]').should('have.text', 'Use Project Default');
+    cy.get('[data-cy=profileEmailData]').should('have.text', userId);
+  });
+});
---
+++ cypress/integration/logout/8-1-logout.spec.ts
@@ -0,0 +1,26 @@
+import * as constants from '../../../src/app/constants/_index';
+import * as api from '../../../src/app/api/_index';
+
+const userId = '8-1-user@example.com';
+const password = '123123';
+
+describe('8-1 logout (logged in)', () => {
+  it(`should be able to logout, redirect to ${constants.PATH_LOGIN}`, () => {
+    cy.deletePack({ user_ids: [userId] });
+    cy.seedPack({
+      users: [
+        {
+          user_id: userId,
+          password: password,
+          email_verified: true
+        }
+      ]
+    });
+    cy.loginUser({ user_id: userId, password: password });
+    cy.basicVisit(constants.PATH_PROFILE);
+    cy.loading();
+    cy.get('[data-cy=userMenuButton]').click();
+    cy.get('[data-cy=logoutButton]').click();
+    cy.url().should('include', constants.PATH_LOGIN);
+  });
+});
