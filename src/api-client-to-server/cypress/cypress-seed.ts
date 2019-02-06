9444a4292c71749472b09dd3934fd14fdbe714fd
akalitenya <akalitenya@mprove.io>
2019-02-06 18:18:52 +0500

    seed

--- src/api-client-to-server/cypress/cypress-seed.ts
+++ src/api-client-to-server/cypress/cypress-seed.ts
@@ -7,6 +7,13 @@ export interface CypressSeedRequestBody {
       email_verification_token?: string;
       password_reset_token?: string;
     }>;
+    projects?: {
+      project_id: string;
+    };
+    members?: {
+      project_id: string;
+      member_id: string;
+    };
   };
 }
 
