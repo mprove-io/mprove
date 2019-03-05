a6de0163fdf36a6eab17185e52f76b87b7a24489
akalitenya <akalitenya@mprove.io>
2019-03-05 16:51:49 +0500

    mono repo p1

--- api/sh/replace-api.sh
+++ api/sh/replace-api.sh
@@ -1,41 +1,16 @@
 #!/usr/bin/env bash
 
-# mprove-backend-node
-rm -rf /Users/akalitenya/mprove-backend-node/src/api
-mkdir /Users/akalitenya/mprove-backend-node/src/api
-cp -rf /Users/akalitenya/mprove-api/src/ /Users/akalitenya/mprove-backend-node/src/api
+# mprove/backend
+rm -rf /Users/akalitenya/mprove/backend/src/api
+mkdir /Users/akalitenya/mprove/backend/src/api
+cp -rf /Users/akalitenya/mprove-api/src/ /Users/akalitenya/mprove/backend/src/api
 
-cp -rf /Users/akalitenya/mprove-api/.prettierrc.yaml /Users/akalitenya/mprove-backend-node/.prettierrc.yaml
-cp -rf /Users/akalitenya/mprove-api/tslint-base.json /Users/akalitenya/mprove-backend-node/tslint-base.json
+# mprove/blockml
+rm -rf /Users/akalitenya/mprove/blockml/src/api
+mkdir /Users/akalitenya/mprove/blockml/src/api
+cp -rf /Users/akalitenya/mprove-api/src/ /Users/akalitenya/mprove/blockml/src/api
 
-# mprove-blockml-node
-rm -rf /Users/akalitenya/mprove-blockml-node/src/api
-mkdir /Users/akalitenya/mprove-blockml-node/src/api
-cp -rf /Users/akalitenya/mprove-api/src/ /Users/akalitenya/mprove-blockml-node/src/api
-
-cp -rf /Users/akalitenya/mprove-api/.prettierrc.yaml /Users/akalitenya/mprove-blockml-node/.prettierrc.yaml
-cp -rf /Users/akalitenya/mprove-api/tslint-base.json /Users/akalitenya/mprove-blockml-node/tslint-base.json
-
-# mprove-web-app
-rm -rf /Users/akalitenya/mprove-web-app/src/app/api
-mkdir /Users/akalitenya/mprove-web-app/src/app/api
-cp -rf /Users/akalitenya/mprove-api/src/ /Users/akalitenya/mprove-web-app/src/app/api
-
-cp -rf /Users/akalitenya/mprove-api/.prettierrc.yaml /Users/akalitenya/mprove-web-app/.prettierrc.yaml
-cp -rf /Users/akalitenya/mprove-api/tslint-base.json /Users/akalitenya/mprove-web-app/tslint-base.json
-cp -rf /Users/akalitenya/mprove-api/tslint-codelyzer.json /Users/akalitenya/mprove-web-app/tslint-codelyzer.json
-
-# mprove-docs-app
-cp -rf /Users/akalitenya/mprove-api/.prettierrc.yaml /Users/akalitenya/mprove-docs-app/.prettierrc.yaml
-cp -rf /Users/akalitenya/mprove-api/tslint-base.json /Users/akalitenya/mprove-docs-app/tslint-base.json
-cp -rf /Users/akalitenya/mprove-api/tslint-codelyzer.json /Users/akalitenya/mprove-docs-app/tslint-codelyzer.json
-
-# mprove-landing-app
-cp -rf /Users/akalitenya/mprove-api/.prettierrc.yaml /Users/akalitenya/mprove-landing-app/.prettierrc.yaml
-cp -rf /Users/akalitenya/mprove-api/tslint-base.json /Users/akalitenya/mprove-landing-app/tslint-base.json
-cp -rf /Users/akalitenya/mprove-api/tslint-codelyzer.json /Users/akalitenya/mprove-landing-app/tslint-codelyzer.json
-
-# mprove-data-generator
-cp -rf /Users/akalitenya/mprove-api/.prettierrc.yaml /Users/akalitenya/mprove-data-generator/.prettierrc.yaml
-cp -rf /Users/akalitenya/mprove-api/tslint-base.json /Users/akalitenya/mprove-data-generator/tslint-base.json
-cp -rf /Users/akalitenya/mprove-api/tslint-codelyzer.json /Users/akalitenya/mprove-data-generator/tslint-codelyzer.json
+# mprove/client
+rm -rf /Users/akalitenya/mprove/client/src/app/api
+mkdir /Users/akalitenya/mprove/client/src/app/api
+cp -rf /Users/akalitenya/mprove-api/src/ /Users/akalitenya/mprove/client/src/app/api
