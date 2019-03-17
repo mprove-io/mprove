5ddc35b3878bca7e524c74d7a0f0d829ab20b38c
akalitenya <akalitenya@mprove.io>
2019-03-17 10:37:37 +0500

    remove sh

--- blockml/vad/build_push_base.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#180724.10
-
-docker build -f docker/base/Dockerfile -t us.gcr.io/mprove-1201/mprove-node-base:180724.10 --no-cache=true .
-
-docker login -u _token -p "$(gcloud auth print-access-token)" https://us.gcr.io
-docker push us.gcr.io/mprove-1201/mprove-node-base:180724.10
--- blockml/vad/tag_list.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker images us.gcr.io/mprove-1201/mprove-blockml-node | grep -v '<none>'
