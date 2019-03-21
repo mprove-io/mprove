1ce1db54f723d8298655bbce5e5eabefd840bbe0
akalitenya <akalitenya@mprove.io>
2019-03-21 14:05:22 +0500

    cleanup blockml create-express

--- blockml/src/create-express.ts
+++ blockml/src/create-express.ts
@@ -2,20 +2,14 @@ import * as errorHandler from 'errorhandler';
 import * as express from 'express';
 import * as lusca from 'lusca';
 import * as logger from 'morgan';
-import * as compression from 'compression'; // compresses requests
+import * as compression from 'compression';
 import * as bodyParser from 'body-parser';
 import expressValidator = require('express-validator');
 import { controllers } from './barrels/controllers';
 
 export function createExpress() {
-  /**
-   * Create Express server.
-   */
   const app = express();
 
-  /**
-   * Express configuration.
-   */
   app.set('port', process.env.BLOCKML_PORT || 8081);
   app.use(compression());
   app.use(logger('dev'));
@@ -25,22 +19,12 @@ export function createExpress() {
   app.use(lusca.xframe('SAMEORIGIN'));
   app.use(lusca.xssProtection(true));
 
-  /**
-   * Primary app routes.
-   */
-
   app.post('/api/v2/processDashboard', controllers.processDashboard);
   app.post('/api/v2/processQuery', controllers.processQuery);
   app.post('/api/v2/rebuildStruct', controllers.rebuildStruct);
 
-  /**
-   * Error Handler. Provides full stack - remove for production
-   */
   app.use(errorHandler());
 
-  /**
-   * Start Express server.
-   */
   app.listen(app.get('port'), () => {
     console.log(
       '  App is running at http://localhost:%d in %s mode',
