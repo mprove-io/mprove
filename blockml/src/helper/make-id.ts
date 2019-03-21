fcf31746a319f787987130a577c40330e263f281
akalitenya <akalitenya@mprove.io>
2019-03-21 14:10:46 +0500

    blockml nanoid length

--- blockml/src/helper/make-id.ts
+++ blockml/src/helper/make-id.ts
@@ -2,5 +2,5 @@ const generate = require('nanoid/generate');
 const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 export function makeId() {
-  return generate(alphabet, 12); // => "V6GE1302MO58"
+  return generate(alphabet, 20); // => "SJ8JVTJWN2TWOY2T6ZJH"
 }
