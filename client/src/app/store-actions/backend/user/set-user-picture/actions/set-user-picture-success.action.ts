5b52ac721fc122162269eeda4c759120f1879c66
akalitenya <akalitenya@mprove.io>
2019-03-22 17:58:36 +0500

    client cleanup set-user-picture-success

--- client/src/app/store-actions/backend/user/set-user-picture/actions/set-user-picture-success.action.ts
+++ client/src/app/store-actions/backend/user/set-user-picture/actions/set-user-picture-success.action.ts
@@ -5,7 +5,5 @@ import * as actionTypes from '@app/store-actions/action-types';
 export class SetUserPictureSuccessAction implements Action {
   readonly type = actionTypes.SET_USER_PICTURE_SUCCESS;
 
-  constructor(public payload: api.SetUserPictureResponse200Body['payload']) {
-    // for effects
-  }
+  constructor(public payload: api.SetUserPictureResponse200Body['payload']) {}
 }
