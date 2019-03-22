dbff22930364a04d130f55c6bc2871b51232b798
akalitenya <akalitenya@mprove.io>
2019-03-22 17:40:06 +0500

    client cleanup group-by.pip3

--- client/src/app/pipes/group-by.pipe.ts
+++ client/src/app/pipes/group-by.pipe.ts
@@ -5,13 +5,13 @@ export class GroupByPipe implements PipeTransform {
   transform(arr: any[], groupKey: any) {
     if (!groupKey) {
       return arr;
-    } // fast return if groupKey is undefined
+    }
     const groupMap = new Map();
     arr.forEach(value => {
       const key = value[groupKey];
       if (!key) {
         return arr;
-      } // fast return if value of key is undefined
+      }
       const subArray = groupMap.get(key);
       if (subArray) {
         groupMap.set(key, [value, ...subArray]);
