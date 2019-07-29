f28a95f9d3e719b91f8ac68f4c2b8bf32f48a9d2
akalitenya <akalitenya@mprove.io>
2019-07-29 10:38:32 +0500

    smtp

--- backend/src/models/helper/send-email.ts
+++ backend/src/models/helper/send-email.ts
@@ -1,14 +1,31 @@
 import * as nodemailer from 'nodemailer';
 import * as mg from 'nodemailer-mailgun-transport';
 
-const auth = {
-  auth: {
-    api_key: process.env.BACKEND_MAILGUN_ACTIVE_API_KEY,
-    domain: process.env.BACKEND_MAILGUN_DOMAIN
-  }
-};
+let emailSender: any;
 
-const emailSender = nodemailer.createTransport(mg(auth));
+if (process.env.BACKEND_NODEMAILER_TRANSPORT === 'SMTP') {
+  let smtpConfig = {
+    host: process.env.BACKEND_SMTP_HOST,
+    port: Number(process.env.BACKEND_SMTP_PORT),
+    secure: process.env.BACKEND_SMTP_SECURE === 'TRUE' ? true : false, // use SSL
+    auth: {
+      user: process.env.BACKEND_SMTP_AUTH_USER,
+      pass: process.env.BACKEND_SMTP_AUTH_PASSWORD
+    }
+  };
+
+  emailSender = nodemailer.createTransport(smtpConfig);
+  //
+} else if (process.env.BACKEND_NODEMAILER_TRANSPORT === 'MAILGUN') {
+  const auth = {
+    auth: {
+      api_key: process.env.BACKEND_MAILGUN_ACTIVE_API_KEY,
+      domain: process.env.BACKEND_MAILGUN_DOMAIN
+    }
+  };
+
+  emailSender = nodemailer.createTransport(mg(auth));
+}
 
 export async function sendEmail(item: {
   to: string;
