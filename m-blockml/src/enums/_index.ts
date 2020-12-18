d7e0aacb6646be720ff43527590cc1b250cca0c4
akalitenya <akalitenya@mprove.io>
2020-12-18 13:37:38 +0500

    blockml enums

--- m-blockml/src/enums/_index.ts
+++ m-blockml/src/enums/_index.ts
@@ -1,11 +1,12 @@
+export * from './docs/fraction-unit.enum';
+export * from './docs/join-type.enum';
+export * from './docs/parameter.enum';
+export * from './docs/time-source.enum';
+export * from './docs/timeframe.enum';
+
 export * from './special/caller.enum';
 export * from './special/er-title.enum';
 export * from './special/func.enum';
 export * from './special/log-type.enum';
-export * from './special/parameter.enum';
 
-export * from './fraction-unit.enum';
-export * from './join-type.enum';
 export * from './time-label.enum';
-export * from './time-source.enum';
-export * from './timeframe.enum';
