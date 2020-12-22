e70a34a1bba7e92bd4def418e6a4ac24feac4562
akalitenya <akalitenya@mprove.io>
2020-12-22 11:58:28 +0500

    blockml process view refs p4

--- m-blockml/src/models/04-view/9-process-view-refs.ts
+++ m-blockml/src/models/04-view/9-process-view-refs.ts
@@ -25,22 +25,25 @@ export function processViewRefs(item: {
 
     x.parts = {};
 
-    let text =
-      Object.keys(x.asDeps).length === 0
-        ? x.derived_table
-        : substituteViewRefsRecursive({
-            topView: x,
-            viewName: x.name,
-            input: x.derived_table,
-            partDeps: {},
-            views: item.views,
-            udfsDict: item.udfsDict,
-            weekStart: item.weekStart,
-            connection: x.connection,
-            structId: item.structId
-          });
+    let input = x.derived_table;
 
-    x.derivedTableStart = text.split('\n');
+    if (Object.keys(x.asDeps).length > 0) {
+      input = api.MyRegex.replaceViewRefs(input, x.name);
+      input = api.MyRegex.removeBracketsOnViewFieldRefs(input);
+
+      substituteViewRefsRecursive({
+        topView: x,
+        view: x,
+        partDeps: {},
+        views: item.views,
+        udfsDict: item.udfsDict,
+        weekStart: item.weekStart,
+        connection: x.connection,
+        structId: item.structId
+      });
+    }
+
+    x.derivedTableStart = input.split('\n');
   });
 
   helper.log(caller, func, structId, enums.LogTypeEnum.Errors, item.errors);
@@ -51,8 +54,7 @@ export function processViewRefs(item: {
 
 function substituteViewRefsRecursive(item: {
   topView: interfaces.View;
-  viewName: string;
-  input: string;
+  view: interfaces.View;
   partDeps: interfaces.ViewPart['deps'];
   views: interfaces.View[];
   udfsDict: api.UdfsDict;
@@ -60,20 +62,15 @@ function substituteViewRefsRecursive(item: {
   connection: api.ProjectConnection;
   structId: string;
 }) {
-  let input = item.input;
+  Object.keys(item.view.asDeps).forEach(as => {
+    let depView = item.views.find(
+      v => v.name === item.view.asDeps[as].viewName
+    );
 
-  let asDeps: interfaces.View['asDeps'] = getAsDeps(input);
-
-  input = api.MyRegex.replaceViewRefs(input, item.viewName);
-  input = api.MyRegex.removeBracketsOnViewFieldRefs(input);
-
-  Object.keys(asDeps).forEach(as => {
-    let depView = item.views.find(v => v.name === asDeps[as].viewName);
-
-    let viewPartName = `${item.viewName}__${depView.name}__${as}`;
+    let viewPartName = `${item.view.name}__${depView.name}__${as}`;
 
     let sub = barSpecial.genSub({
-      select: Object.keys(asDeps[as].fieldNames),
+      select: Object.keys(item.view.asDeps[as].fieldNames),
       view: depView,
       udfsDict: item.udfsDict,
       weekStart: item.weekStart,
@@ -106,16 +103,11 @@ function substituteViewRefsRecursive(item: {
 
     item.topView.parts[viewPartName] = viewPart;
 
-    let newInput = content.join('\n');
-
-    let newAsDeps: interfaces.View['asDeps'] = getAsDeps(newInput);
-
-    if (Object.keys(newAsDeps).length > 0) {
+    if (Object.keys(depView.asDeps).length > 0) {
       substituteViewRefsRecursive({
         topView: item.topView,
-        viewName: depView.name,
+        view: depView,
         partDeps: viewPart.deps,
-        input: newInput,
         views: item.views,
         udfsDict: item.udfsDict,
         weekStart: item.weekStart,
@@ -124,34 +116,5 @@ function substituteViewRefsRecursive(item: {
       });
     }
   });
-
-  return input;
-}
-
-function getAsDeps(input: string) {
-  let asDeps: interfaces.View['asDeps'] = {};
-
-  let reg = api.MyRegex.CAPTURE_VIEW_REF_G();
-  let r;
-
-  while ((r = reg.exec(input))) {
-    let view: string = r[1];
-    let alias: string = r[2];
-
-    if (helper.isUndefined(asDeps[alias])) {
-      asDeps[alias] = { viewName: view, fieldNames: {} };
-    }
-  }
-
-  let reg2 = api.MyRegex.CAPTURE_DOUBLE_REF_G();
-  let r2;
-
-  while ((r2 = reg2.exec(input))) {
-    let as: string = r2[1];
-    let dep: string = r2[2];
-
-    asDeps[as].fieldNames[dep] = 1;
-  }
-
-  return asDeps;
+  return;
 }
