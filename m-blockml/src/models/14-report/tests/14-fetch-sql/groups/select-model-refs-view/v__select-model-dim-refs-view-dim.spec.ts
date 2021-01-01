1191fb39d01323422923a1061595a44d84aee22a
akalitenya <akalitenya@mprove.io>
2021-01-01 17:41:17 +0500

    blockml gen sql tests p2

---
+++ m-blockml/src/models/14-report/tests/14-fetch-sql/groups/select-model-refs-view/v__select-model-dim-refs-view-dim.spec.ts
@@ -0,0 +1,73 @@
+import { api } from '../../../../../../barrels/api';
+import { enums } from '../../../../../../barrels/enums';
+import { interfaces } from '../../../../../../barrels/interfaces';
+import { helper } from '../../../../../../barrels/helper';
+import { prepareTest } from '../../../../../../functions/prepare-test';
+import { BmError } from '../../../../../../models/bm-error';
+import * as fse from 'fs-extra';
+
+let caller = enums.CallerEnum.BuildDashboardReport;
+let func = enums.FuncEnum.FetchSql;
+let testId = 'groups/select-model-refs-view/v__select-model-dim-refs-view-dim';
+
+test(testId, async () => {
+  let errors: BmError[];
+  let entDashboards: interfaces.Dashboard[];
+
+  try {
+    let {
+      structService,
+      traceId,
+      structId,
+      dataDir,
+      fromDir,
+      toDir
+    } = await prepareTest(caller, func, testId);
+
+    let connection: api.ProjectConnection = {
+      name: 'c1',
+      type: api.ConnectionTypeEnum.BigQuery
+    };
+
+    await structService.rebuildStruct({
+      traceId: traceId,
+      dir: dataDir,
+      structId: structId,
+      connections: [connection],
+      weekStart: api.ProjectWeekStartEnum.Monday
+    });
+
+    errors = await helper.readLog(fromDir, enums.LogTypeEnum.Errors);
+    entDashboards = await helper.readLog(fromDir, enums.LogTypeEnum.Entities);
+    fse.copySync(fromDir, toDir);
+  } catch (e) {
+    api.logToConsole(e);
+  }
+
+  expect(errors.length).toBe(0);
+  expect(entDashboards.length).toBe(1);
+
+  expect(entDashboards[0].reports[0].sql).toEqual([
+    '#standardSQL',
+    'WITH',
+    '  derived__v1__a AS (',
+    '    SELECT d1',
+    '    FROM tab1',
+    '  ),',
+    '  view__v1__a AS (',
+    '    SELECT',
+    "      FORMAT_TIMESTAMP('%F %H', (d1) + 1) as time1___hour",
+    '    FROM derived__v1__a',
+    '  ),',
+    '  main AS (',
+    '    SELECT',
+    '      a.time1___hour + 2 as mf_dim2',
+    '    FROM view__v1__a as a',
+    '    GROUP BY 1',
+    '  )',
+    'SELECT',
+    '  mf_dim2',
+    'FROM main',
+    'LIMIT 500'
+  ]);
+});
---
+++ m-blockml/src/models/14-report/tests/14-fetch-sql/groups/sql-on/v__sql-on-refs-model-dim-2.spec.ts
@@ -0,0 +1,84 @@
+import { api } from '../../../../../../barrels/api';
+import { enums } from '../../../../../../barrels/enums';
+import { interfaces } from '../../../../../../barrels/interfaces';
+import { helper } from '../../../../../../barrels/helper';
+import { prepareTest } from '../../../../../../functions/prepare-test';
+import { BmError } from '../../../../../../models/bm-error';
+import * as fse from 'fs-extra';
+
+let caller = enums.CallerEnum.BuildDashboardReport;
+let func = enums.FuncEnum.FetchSql;
+let testId = 'groups/sql-on/v__sql-on-refs-model-dim-2';
+
+test(testId, async () => {
+  let errors: BmError[];
+  let entDashboards: interfaces.Dashboard[];
+
+  try {
+    let {
+      structService,
+      traceId,
+      structId,
+      dataDir,
+      fromDir,
+      toDir
+    } = await prepareTest(caller, func, testId);
+
+    let connection: api.ProjectConnection = {
+      name: 'c1',
+      type: api.ConnectionTypeEnum.BigQuery
+    };
+
+    await structService.rebuildStruct({
+      traceId: traceId,
+      dir: dataDir,
+      structId: structId,
+      connections: [connection],
+      weekStart: api.ProjectWeekStartEnum.Monday
+    });
+
+    errors = await helper.readLog(fromDir, enums.LogTypeEnum.Errors);
+    entDashboards = await helper.readLog(fromDir, enums.LogTypeEnum.Entities);
+    fse.copySync(fromDir, toDir);
+  } catch (e) {
+    api.logToConsole(e);
+  }
+
+  expect(errors.length).toBe(0);
+  expect(entDashboards.length).toBe(1);
+
+  expect(entDashboards[0].reports[0].sql).toEqual([
+    '#standardSQL',
+    'WITH',
+    '  derived__v1__a AS (',
+    '    SELECT d1, d3, d5',
+    '    FROM tab1',
+    '  ),',
+    '  derived__v1__b AS (',
+    '    SELECT d1, d3, d5',
+    '    FROM tab1',
+    '  ),',
+    '  view__v1__a AS (',
+    '    SELECT',
+    "      FORMAT_TIMESTAMP('%F %H', (d1) + 1) as time1___hour",
+    '    FROM derived__v1__a',
+    '  ),',
+    '  view__v1__b AS (',
+    '    SELECT',
+    '      d5 as dim5,',
+    '      d3 as dim3',
+    '    FROM derived__v1__b',
+    '  ),',
+    '  main AS (',
+    '    SELECT',
+    '      b.dim5 as b_dim5',
+    '    FROM view__v1__a as a',
+    '    LEFT OUTER JOIN view__v1__b as b ON (a.time1___hour + 2) = (b.dim3 + 4)',
+    '    GROUP BY 1',
+    '  )',
+    'SELECT',
+    '  b_dim5',
+    'FROM main',
+    'LIMIT 500'
+  ]);
+});
