93e87146917d5b05892fe11bdcd0712cb19e7c36
akalitenya <akalitenya@mprove.io>
2021-01-03 13:01:05 +0500

    blockml gen sql tests p4

---
+++ m-blockml/src/models/14-report/tests/14-fetch-sql/groups/sql-always-where-calc/v__sql-always-where-calc-refs-view-calc.spec.ts
@@ -0,0 +1,81 @@
+import { api } from '../../../../../../barrels/api';
+import { enums } from '../../../../../../barrels/enums';
+import { interfaces } from '../../../../../../barrels/interfaces';
+import { helper } from '../../../../../../barrels/helper';
+import { prepareTest } from '../../../../../../functions/prepare-test';
+import { BmError } from '../../../../../../models/bm-error';
+import * as fse from 'fs-extra';
+
+let caller = enums.CallerEnum.BuildDashboardReport;
+let func = enums.FuncEnum.FetchSql;
+let testId =
+  'groups/sql-always-where-calc/v__sql-always-where-calc-refs-view-calc';
+
+test(testId, async () => {
+  let errors: BmError[];
+  let entDashboards: interfaces.Dashboard[];
+
+  try {
+    let {
+      structService,
+      traceId,
+      structId,
+      dataDir,
+      fromDir,
+      toDir
+    } = await prepareTest(caller, func, testId);
+
+    let connection: api.ProjectConnection = {
+      name: 'c1',
+      type: api.ConnectionTypeEnum.BigQuery
+    };
+
+    await structService.rebuildStruct({
+      traceId: traceId,
+      dir: dataDir,
+      structId: structId,
+      connections: [connection],
+      weekStart: api.ProjectWeekStartEnum.Monday
+    });
+
+    errors = await helper.readLog(fromDir, enums.LogTypeEnum.Errors);
+    entDashboards = await helper.readLog(fromDir, enums.LogTypeEnum.Entities);
+    fse.copySync(fromDir, toDir);
+  } catch (e) {
+    api.logToConsole(e);
+  }
+
+  expect(errors.length).toBe(0);
+  expect(entDashboards.length).toBe(1);
+
+  expect(entDashboards[0].reports[0].sql).toEqual([
+    '#standardSQL',
+    'CREATE TEMPORARY FUNCTION mprove_array_sum(ar ARRAY<STRING>) AS',
+    "  ((SELECT SUM(CAST(REGEXP_EXTRACT(val, '\\\\|\\\\|(\\\\-?\\\\d+(?:.\\\\d+)?)$') AS FLOAT64)) FROM UNNEST(ar) as val));",
+    'WITH',
+    '  derived__v1__a AS (',
+    '    SELECT d1, d3, d5',
+    '    FROM tab1',
+    '  ),',
+    '  view__v1__a AS (',
+    '    SELECT',
+    '      d5 as dim5,',
+    "      (FORMAT_TIMESTAMP('%F %H', (d1) + 1)) + 2 as dim2,",
+    '      (d3) + 4 as dim4',
+    '    FROM derived__v1__a',
+    '  ),',
+    '  main AS (',
+    '    SELECT',
+    '      a.dim5 as a_dim5,',
+    "      COALESCE(mprove_array_sum(ARRAY_AGG(DISTINCT CONCAT(CONCAT(CAST(a.dim4 + mk1 AS STRING), '||'), CAST(a.dim2 + ms1 AS STRING)))), 0) as a_mea1",
+    '    FROM view__v1__a as a',
+    '    GROUP BY 1',
+    '  )',
+    'SELECT',
+    '  a_dim5',
+    'FROM main',
+    'WHERE',
+    '  (((a_mea1 + 1) + 2) > 100)',
+    'LIMIT 500'
+  ]);
+});
