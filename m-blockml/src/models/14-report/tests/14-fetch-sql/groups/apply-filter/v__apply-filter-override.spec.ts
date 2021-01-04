1f2466d7ea00d1e8f569d08b667d3c12a6cd0f4a
akalitenya <akalitenya@mprove.io>
2021-01-04 12:41:13 +0500

    blockml gen sql tests p7

---
+++ m-blockml/src/models/14-report/tests/14-fetch-sql/groups/apply-filter/v__apply-filter-override.spec.ts
@@ -0,0 +1,85 @@
+import { api } from '../../../../../../barrels/api';
+import { enums } from '../../../../../../barrels/enums';
+import { interfaces } from '../../../../../../barrels/interfaces';
+import { helper } from '../../../../../../barrels/helper';
+import { prepareTest } from '../../../../../../functions/prepare-test';
+import { BmError } from '../../../../../../models/bm-error';
+import * as fse from 'fs-extra';
+
+let caller = enums.CallerEnum.BuildDashboardReport;
+let func = enums.FuncEnum.FetchSql;
+let testId = 'groups/apply-filter/v__apply-filter-override';
+
+test(testId, async () => {
+  let errors: BmError[];
+  let entDashboards: interfaces.Dashboard[];
+
+  try {
+    let {
+      structService,
+      traceId,
+      structId,
+      dataDir,
+      fromDir,
+      toDir
+    } = await prepareTest(caller, func, testId);
+
+    let connection: api.ProjectConnection = {
+      name: 'c1',
+      type: api.ConnectionTypeEnum.BigQuery
+    };
+
+    await structService.rebuildStruct({
+      traceId: traceId,
+      dir: dataDir,
+      structId: structId,
+      connections: [connection],
+      weekStart: api.ProjectWeekStartEnum.Monday
+    });
+
+    errors = await helper.readLog(fromDir, enums.LogTypeEnum.Errors);
+    entDashboards = await helper.readLog(fromDir, enums.LogTypeEnum.Entities);
+    fse.copySync(fromDir, toDir);
+  } catch (e) {
+    api.logToConsole(e);
+  }
+
+  expect(errors.length).toBe(0);
+  expect(entDashboards.length).toBe(1);
+
+  expect(entDashboards[0].reports[0].sql).toEqual([
+    '#standardSQL',
+    'WITH',
+    '  derived__v1__a AS (',
+    '    SELECT d1',
+    '    FROM tab1',
+    '    WHERE (target > 50)',
+    '  ),',
+    '  derived__v1__b AS (',
+    '    SELECT d1',
+    '    FROM tab1',
+    '    WHERE (target > 100)',
+    '  ),',
+    '  view__v1__a AS (',
+    '    SELECT',
+    '      d1 as dim1',
+    '    FROM derived__v1__a',
+    '  ),',
+    '  view__v1__b AS (',
+    '    SELECT',
+    '      d1 as dim1',
+    '    FROM derived__v1__b',
+    '  ),',
+    '  main AS (',
+    '    SELECT',
+    '      b.dim1 as b_dim1',
+    '    FROM view__v1__a as a',
+    '    LEFT OUTER JOIN view__v1__b as b ON a.dim1 = b.dim1',
+    '    GROUP BY 1',
+    '  )',
+    'SELECT',
+    '  b_dim1',
+    'FROM main',
+    'LIMIT 500'
+  ]);
+});
---
+++ m-blockml/src/models/14-report/tests/14-fetch-sql/groups/apply-filter/v__apply-filter-to-sql-always-where.spec.ts
@@ -0,0 +1,71 @@
+import { api } from '../../../../../../barrels/api';
+import { enums } from '../../../../../../barrels/enums';
+import { interfaces } from '../../../../../../barrels/interfaces';
+import { helper } from '../../../../../../barrels/helper';
+import { prepareTest } from '../../../../../../functions/prepare-test';
+import { BmError } from '../../../../../../models/bm-error';
+import * as fse from 'fs-extra';
+
+let caller = enums.CallerEnum.BuildDashboardReport;
+let func = enums.FuncEnum.FetchSql;
+let testId = 'groups/apply-filter/v__apply-filter-to-sql-always-where';
+
+test(testId, async () => {
+  let errors: BmError[];
+  let entDashboards: interfaces.Dashboard[];
+
+  try {
+    let {
+      structService,
+      traceId,
+      structId,
+      dataDir,
+      fromDir,
+      toDir
+    } = await prepareTest(caller, func, testId);
+
+    let connection: api.ProjectConnection = {
+      name: 'c1',
+      type: api.ConnectionTypeEnum.BigQuery
+    };
+
+    await structService.rebuildStruct({
+      traceId: traceId,
+      dir: dataDir,
+      structId: structId,
+      connections: [connection],
+      weekStart: api.ProjectWeekStartEnum.Monday
+    });
+
+    errors = await helper.readLog(fromDir, enums.LogTypeEnum.Errors);
+    entDashboards = await helper.readLog(fromDir, enums.LogTypeEnum.Entities);
+    fse.copySync(fromDir, toDir);
+  } catch (e) {
+    api.logToConsole(e);
+  }
+
+  expect(errors.length).toBe(0);
+  expect(entDashboards.length).toBe(1);
+
+  expect(entDashboards[0].reports[0].sql).toEqual([
+    '#standardSQL',
+    'WITH',
+    '  view__v1__a AS (',
+    '    SELECT',
+    '      d1 as dim1',
+    '    FROM `tab1`',
+    '  ),',
+    '  main AS (',
+    '    SELECT',
+    '      a.dim1 as a_dim1',
+    '    FROM view__v1__a as a',
+    '    WHERE',
+    '      ((target > 100))',
+    '    GROUP BY 1',
+    '  )',
+    'SELECT',
+    '  a_dim1',
+    'FROM main',
+    'LIMIT 500'
+  ]);
+});
