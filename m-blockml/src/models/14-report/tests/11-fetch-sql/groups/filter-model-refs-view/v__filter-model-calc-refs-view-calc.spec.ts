0cdd0eac80a45ad233feff8995ab6aa61cd85e90
akalitenya <akalitenya@mprove.io>
2021-01-10 14:41:10 +0500

    blockml tests filters p2

---
+++ m-blockml/src/models/14-report/tests/11-fetch-sql/groups/filter-model-refs-view/v__filter-model-calc-refs-view-calc.spec.ts
@@ -0,0 +1,145 @@
+import { api } from '../../../../../../barrels/api';
+import { enums } from '../../../../../../barrels/enums';
+import { interfaces } from '../../../../../../barrels/interfaces';
+import { helper } from '../../../../../../barrels/helper';
+import { prepareTest } from '../../../../../../functions/prepare-test';
+import { BmError } from '../../../../../../models/bm-error';
+import * as fse from 'fs-extra';
+
+let caller = enums.CallerEnum.BuildDashboardReport;
+let func = enums.FuncEnum.FetchSql;
+let testId =
+  'groups/filter-model-refs-view/v__filter-model-calc-refs-view-calc';
+
+test('1', async () => {
+  let errors: BmError[];
+  let entDashboards: interfaces.Dashboard[];
+
+  try {
+    let connection: api.ProjectConnection = {
+      name: 'c1',
+      type: api.ConnectionTypeEnum.BigQuery
+    };
+
+    let {
+      structService,
+      traceId,
+      structId,
+      dataDir,
+      fromDir,
+      toDir
+    } = await prepareTest(caller, func, testId, connection);
+
+    await structService.rebuildStruct({
+      traceId: traceId,
+      dir: dataDir,
+      structId: structId,
+      connections: [connection],
+      weekStart: api.ProjectWeekStartEnum.Monday
+    });
+
+    errors = await helper.readLog(fromDir, enums.LogTypeEnum.Errors);
+    entDashboards = await helper.readLog(fromDir, enums.LogTypeEnum.Entities);
+    fse.copySync(fromDir, toDir);
+  } catch (e) {
+    api.logToConsole(e);
+  }
+
+  let sql = `#standardSQL
+CREATE TEMPORARY FUNCTION mprove_array_sum(ar ARRAY<STRING>) AS
+  ((SELECT SUM(CAST(REGEXP_EXTRACT(val, '\\\\|\\\\|(\\\\-?\\\\d+(?:.\\\\d+)?)$') AS FLOAT64)) FROM UNNEST(ar) as val));
+WITH
+  derived__v1__a AS (
+    SELECT d1, d5
+    FROM tab1
+  ),
+  view__v1__a AS (
+    SELECT
+      d5 as dim5,
+      (FORMAT_TIMESTAMP('%F %H', (d1) + 1)) + 2 as dim2,
+      (d3) + 4 as dim4
+    FROM derived__v1__a
+  ),
+  main AS (
+    SELECT
+      a.dim5 as a_dim5,
+      COALESCE(mprove_array_sum(ARRAY_AGG(DISTINCT CONCAT(CONCAT(CAST(a.dim4 + mk1 AS STRING), '||'), CAST(a.dim2 + ms1 AS STRING)))), 0) as a_mea1
+    FROM view__v1__a as a
+    GROUP BY 1
+  )
+SELECT
+  a_dim5
+FROM main
+WHERE
+  ((a_mea1 + 1) + 2 > 100)
+LIMIT 500`;
+
+  expect(errors.length).toBe(0);
+  expect(entDashboards.length).toBe(1);
+  expect(entDashboards[0].reports[0].sql.join('\n')).toEqual(sql);
+});
+
+test('2', async () => {
+  let errors: BmError[];
+  let entDashboards: interfaces.Dashboard[];
+
+  try {
+    let connection: api.ProjectConnection = {
+      name: 'c1',
+      type: api.ConnectionTypeEnum.PostgreSQL
+    };
+
+    let {
+      structService,
+      traceId,
+      structId,
+      dataDir,
+      fromDir,
+      toDir
+    } = await prepareTest(caller, func, testId, connection);
+
+    await structService.rebuildStruct({
+      traceId: traceId,
+      dir: dataDir,
+      structId: structId,
+      connections: [connection],
+      weekStart: api.ProjectWeekStartEnum.Monday
+    });
+
+    errors = await helper.readLog(fromDir, enums.LogTypeEnum.Errors);
+    entDashboards = await helper.readLog(fromDir, enums.LogTypeEnum.Entities);
+    fse.copySync(fromDir, toDir);
+  } catch (e) {
+    api.logToConsole(e);
+  }
+
+  let sql = `WITH
+  derived__v1__a AS (
+    SELECT d1, d5
+    FROM tab1
+  ),
+  view__v1__a AS (
+    SELECT
+      d5 as dim5,
+      (TO_CHAR(DATE_TRUNC('hour', (d1) + 1), 'YYYY-MM-DD HH24')) + 2 as dim2,
+      (d3) + 4 as dim4
+    FROM derived__v1__a
+  ),
+  main AS (
+    SELECT
+      a.dim5 as a_dim5,
+      COALESCE(COALESCE(CAST((SUM(DISTINCT CAST(FLOOR(COALESCE(a.dim2 + ms1, 0)*(1000000*1.0)) AS DECIMAL(38,0)) + CAST(('x' || lpad(LEFT(MD5(CAST(a.dim4 + mk1 AS VARCHAR)),15), 16, '0'))::bit(64)::bigint AS DECIMAL(38,0))* 1.0e8 + CAST(('x' || lpad(RIGHT(MD5(CAST(a.dim4 + mk1 AS VARCHAR)),15), 16, '0'))::bit(64)::bigint AS DECIMAL(38,0))) - SUM(DISTINCT CAST(('x' || lpad(LEFT(MD5(CAST(a.dim4 + mk1 AS VARCHAR)),15), 16, '0'))::bit(64)::bigint AS DECIMAL(38,0))* 1.0e8 + CAST(('x' || lpad(RIGHT(MD5(CAST(a.dim4 + mk1 AS VARCHAR)),15), 16, '0'))::bit(64)::bigint AS DECIMAL(38,0)))) AS DOUBLE PRECISION) / CAST((1000000*1.0) AS DOUBLE PRECISION), 0), 0) as a_mea1
+    FROM view__v1__a as a
+    GROUP BY 1
+  )
+SELECT
+  a_dim5
+FROM main
+WHERE
+  ((a_mea1 + 1) + 2 > 100)
+LIMIT 500`;
+
+  expect(errors.length).toBe(0);
+  expect(entDashboards.length).toBe(1);
+  expect(entDashboards[0].reports[0].sql.join('\n')).toEqual(sql);
+});
