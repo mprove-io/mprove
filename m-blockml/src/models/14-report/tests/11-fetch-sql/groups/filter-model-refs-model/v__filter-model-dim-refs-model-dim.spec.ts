1faab8dc0d7adfe88b0bbd14905bf6879877eb10
akalitenya <akalitenya@mprove.io>
2021-01-10 14:57:47 +0500

    blockml tests filters p3

---
+++ m-blockml/src/models/14-report/tests/11-fetch-sql/groups/filter-model-refs-model/v__filter-model-dim-refs-model-dim.spec.ts
@@ -0,0 +1,137 @@
+import { api } from '../../../../../../barrels/api';
+import { enums } from '../../../../../../barrels/enums';
+import { interfaces } from '../../../../../../barrels/interfaces';
+import { helper } from '../../../../../../barrels/helper';
+import { prepareTest } from '../../../../../../functions/prepare-test';
+import { BmError } from '../../../../../../models/bm-error';
+import * as fse from 'fs-extra';
+
+let caller = enums.CallerEnum.BuildDashboardReport;
+let func = enums.FuncEnum.FetchSql;
+let testId =
+  'groups/filter-model-refs-model/v__filter-model-dim-refs-model-dim';
+
+test('1', async () => {
+  let errors: BmError[];
+  let entDashboards: interfaces.Dashboard[];
+
+  try {
+    let connection: api.ProjectConnection = {
+      name: 'c1',
+      type: api.ConnectionTypeEnum.BigQuery
+    };
+
+    let {
+      structService,
+      traceId,
+      structId,
+      dataDir,
+      fromDir,
+      toDir
+    } = await prepareTest(caller, func, testId, connection);
+
+    await structService.rebuildStruct({
+      traceId: traceId,
+      dir: dataDir,
+      structId: structId,
+      connections: [connection],
+      weekStart: api.ProjectWeekStartEnum.Monday
+    });
+
+    errors = await helper.readLog(fromDir, enums.LogTypeEnum.Errors);
+    entDashboards = await helper.readLog(fromDir, enums.LogTypeEnum.Entities);
+    fse.copySync(fromDir, toDir);
+  } catch (e) {
+    api.logToConsole(e);
+  }
+
+  let sql = `#standardSQL
+WITH
+  derived__v1__a AS (
+    SELECT d5
+    FROM tab1
+  ),
+  view__v1__a AS (
+    SELECT
+      d5 as dim5
+    FROM derived__v1__a
+  ),
+  main AS (
+    SELECT
+      a.dim5 as a_dim5
+    FROM view__v1__a as a
+    WHERE
+      ((FORMAT_TIMESTAMP('%F %H', (d1) + 1)) + 2 = 'bar')
+    GROUP BY 1
+  )
+SELECT
+  a_dim5
+FROM main
+LIMIT 500`;
+
+  expect(errors.length).toBe(0);
+  expect(entDashboards.length).toBe(1);
+  expect(entDashboards[0].reports[0].sql.join('\n')).toEqual(sql);
+});
+
+test('2', async () => {
+  let errors: BmError[];
+  let entDashboards: interfaces.Dashboard[];
+
+  try {
+    let connection: api.ProjectConnection = {
+      name: 'c1',
+      type: api.ConnectionTypeEnum.PostgreSQL
+    };
+
+    let {
+      structService,
+      traceId,
+      structId,
+      dataDir,
+      fromDir,
+      toDir
+    } = await prepareTest(caller, func, testId, connection);
+
+    await structService.rebuildStruct({
+      traceId: traceId,
+      dir: dataDir,
+      structId: structId,
+      connections: [connection],
+      weekStart: api.ProjectWeekStartEnum.Monday
+    });
+
+    errors = await helper.readLog(fromDir, enums.LogTypeEnum.Errors);
+    entDashboards = await helper.readLog(fromDir, enums.LogTypeEnum.Entities);
+    fse.copySync(fromDir, toDir);
+  } catch (e) {
+    api.logToConsole(e);
+  }
+
+  let sql = `WITH
+  derived__v1__a AS (
+    SELECT d5
+    FROM tab1
+  ),
+  view__v1__a AS (
+    SELECT
+      d5 as dim5
+    FROM derived__v1__a
+  ),
+  main AS (
+    SELECT
+      a.dim5 as a_dim5
+    FROM view__v1__a as a
+    WHERE
+      ((TO_CHAR(DATE_TRUNC('hour', (d1) + 1), 'YYYY-MM-DD HH24')) + 2 = 'bar')
+    GROUP BY 1
+  )
+SELECT
+  a_dim5
+FROM main
+LIMIT 500`;
+
+  expect(errors.length).toBe(0);
+  expect(entDashboards.length).toBe(1);
+  expect(entDashboards[0].reports[0].sql.join('\n')).toEqual(sql);
+});
