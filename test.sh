94762d8254f3adf6437ac77a3716c7447949a7a0
akalitenya <akalitenya@mprove.io>
2021-01-21 14:27:59 +0500

    test script

--- test.sh
+++ test.sh
@@ -1,14 +1,31 @@
-#
-failures=0
-trap 'failures=$((failures+1))' ERR
-(cd "m-backend" && yarn test)
-(cd "m-disk" && yarn test)
-(cd "m-blockml" && yarn test)
-if ((failures == 0)); then
-  echo "Success"
-else
-  echo "$failures failures"
+#!/bin/bash
+
+backendOutput="$(cd "m-backend" && yarn test)"
+if [[ $? != 0 ]]
+then
+  echo "$backendOutput"
+  echo "backend Fail!"
   exit 1
+else 
+  echo "backend Success!"
 fi
 
+diskOutput="$(cd "m-disk" && yarn test)"
+if [[ $? != 0 ]]
+then
+  echo "$diskOutput"
+  echo "disk Fail!"
+  exit 1
+else 
+  echo "disk Success!"
+fi
 
+blockmlOutput="$(cd "m-blockml" && yarn test)"
+if [[ $? != 0 ]]
+then
+  echo "$blockmlOutput"
+  echo "blockml Fail!"
+  exit 1
+else 
+  echo "blockml Success!"
+fi
