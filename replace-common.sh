a97e6b928d7e224e846607718df87791ee77c9f7
akalitenya <akalitenya@mprove.io>
2021-01-25 18:23:30 +0500

    paths p3

--- replace-common.sh
+++ replace-common.sh
@@ -7,16 +7,9 @@
 # from m-common ESLINT
 #
 
-# to m-common/api
-cp -rf m-common/eslint/.eslintrc.js m-common/api/.eslintrc.js
-
-# to m-backend
+cp -rf m-common/eslint/.eslintrc.js m-common/ts/.eslintrc.js
 cp -rf m-common/eslint/.eslintrc.js m-backend/.eslintrc.js
-
-# to m-disk
 cp -rf m-common/eslint/.eslintrc.js m-disk/.eslintrc.js
-
-# to m-blockml
 cp -rf m-common/eslint/.eslintrc.js m-blockml/.eslintrc.js
 
 #
@@ -25,37 +18,26 @@ cp -rf m-common/eslint/.eslintrc.js m-blockml/.eslintrc.js
 
 # to root
 cp -rf m-common/prettier/.prettierrc.yaml .prettierrc.yaml
-
-# to m-common/api
-cp -rf m-common/prettier/.prettierrc.yaml m-common/api/.prettierrc.yaml
-
-# to m-backend
+cp -rf m-common/prettier/.prettierrc.yaml m-common/ts/.prettierrc.yaml
 cp -rf m-common/prettier/.prettierrc.yaml m-backend/.prettierrc.yaml
-
-# to m-disk
 cp -rf m-common/prettier/.prettierrc.yaml m-disk/.prettierrc.yaml
-
-# to m-blockml
 cp -rf m-common/prettier/.prettierrc.yaml m-blockml/.prettierrc.yaml
 
 #
-# from m-common API
+# from m-common TS
 #
 
-# to m-backend
 rm -rf m-backend/src/api
 mkdir m-backend/src/api
-cp -rf m-common/api/src/ m-backend/src/api
+cp -rf m-common/ts/src/ m-backend/src/api
 
-# to m-disk
 rm -rf m-disk/src/api
 mkdir m-disk/src/api
-cp -rf m-common/api/src/ m-disk/src/api
+cp -rf m-common/ts/src/api m-disk/src/api
 
-# to m-blockml
 rm -rf m-blockml/src/api
 mkdir m-blockml/src/api
-cp -rf m-common/api/src/ m-blockml/src/api
+cp -rf m-common/ts/src/ m-blockml/src/api
 
 ############################## OLD ##############################
 
