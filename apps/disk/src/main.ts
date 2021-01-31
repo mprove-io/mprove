b54651389f54948ea8b2aeac2ab9d7a7ff4d4838
akalitenya <akalitenya@mprove.io>
2021-01-31 14:35:58 +0500

    nx p12

--- apps/disk/src/main.ts
+++ apps/disk/src/main.ts
@@ -1,20 +1,12 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import 'module-alias/register';
+import { AppModule } from './app.module';
+import { api } from './barrels/api';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
-  });
-}
+  api.listenProcessEvents();
 
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3002);
+}
 bootstrap();
