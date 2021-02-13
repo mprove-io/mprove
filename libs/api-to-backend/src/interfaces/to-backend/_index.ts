fe81f8b7af78c54717ece94a36162497795d50e4
akalitenya <akalitenya@mprove.io>
2021-02-13 15:30:28 +0500

    backend api projects p1

--- libs/api-to-backend/src/interfaces/to-backend/_index.ts
+++ libs/api-to-backend/src/interfaces/to-backend/_index.ts
@@ -1,4 +1,6 @@
+export * from './org-users/_index';
 export * from './orgs/_index';
+export * from './projects/_index';
 export * from './records/_index';
 export * from './special/_index';
 export * from './to-backend-request';
