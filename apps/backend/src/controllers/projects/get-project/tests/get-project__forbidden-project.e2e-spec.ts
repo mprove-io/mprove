273e4bcb4eced2a7937e2b41767e7bd33de6376b
akalitenya <akalitenya@mprove.io>
2021-02-22 15:17:34 +0500

    backend test get project p2

---
+++ apps/backend/src/controllers/projects/get-project/tests/get-project__forbidden-project.e2e-spec.ts
@@ -0,0 +1,92 @@
+import test from 'ava';
+import { apiToBackend } from '~backend/barrels/api-to-backend';
+import { common } from '~backend/barrels/common';
+import { helper } from '~backend/barrels/helper';
+import { interfaces } from '~backend/barrels/interfaces';
+import { prepareTest } from '~backend/functions/prepare-test';
+
+let testId = 'get-project__forbidden-project';
+
+let traceId = testId;
+let email = `${testId}@example.com`;
+let password = '123';
+let orgName = testId;
+let userId = common.makeId();
+let orgId = common.makeId();
+let projectName = testId;
+let projectId = common.makeId();
+let prep: interfaces.Prep;
+
+test('1', async t => {
+  let resp: apiToBackend.ToBackendGetProjectResponse;
+
+  try {
+    prep = await prepareTest({
+      traceId: traceId,
+      deleteRecordsPayload: {
+        emails: [email],
+        orgNames: [orgName],
+        projectNames: [projectName]
+      },
+      seedRecordsPayload: {
+        users: [
+          {
+            userId,
+            email,
+            password,
+            isEmailVerified: common.BoolEnum.TRUE
+          }
+        ],
+        orgs: [
+          {
+            orgId,
+            name: orgName,
+            ownerEmail: email
+          }
+        ],
+        projects: [
+          {
+            orgId,
+            projectId,
+            name: projectName
+          }
+        ],
+        members: [
+          {
+            memberId: userId,
+            email,
+            projectId,
+            isAdmin: common.BoolEnum.FALSE,
+            isEditor: common.BoolEnum.TRUE,
+            isExplorer: common.BoolEnum.TRUE
+          }
+        ]
+      },
+      loginUserPayload: { email, password }
+    });
+
+    let req: apiToBackend.ToBackendGetProjectRequest = {
+      info: {
+        name: apiToBackend.ToBackendRequestInfoNameEnum.ToBackendGetProject,
+        traceId: traceId
+      },
+      payload: {
+        projectId: projectId
+      }
+    };
+
+    resp = await helper.sendToBackend<apiToBackend.ToBackendGetProjectResponse>(
+      {
+        httpServer: prep.httpServer,
+        loginToken: prep.loginToken,
+        req: req
+      }
+    );
+
+    await prep.app.close();
+  } catch (e) {
+    common.logToConsole(e);
+  }
+
+  t.is(resp.info.error.message, apiToBackend.ErEnum.BACKEND_FORBIDDEN_PROJECT);
+});
