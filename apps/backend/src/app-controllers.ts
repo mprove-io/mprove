be3c78c3beff2458596aa595d5df65ef4754239c
akalitenya <akalitenya@mprove.io>
2021-03-22 16:20:05 +0500

    backend app controllers

--- apps/backend/src/app-controllers.ts
+++ apps/backend/src/app-controllers.ts
@@ -1,101 +1,101 @@
 import { controllers } from './barrels/controllers';
 
 export const appControllers = [
-  controllers.DeleteRecordsController,
-  controllers.SeedRecordsController,
+  controllers.GetAvatarBigController,
+  controllers.SetAvatarController,
 
-  controllers.RebuildStructSpecialController,
-
-  controllers.ConfirmUserEmailController,
-  controllers.GetUserProfileController,
-  controllers.LoginUserController,
-  controllers.LogoutUserController,
-  controllers.RegisterUserController,
-  controllers.SetUserNameController,
-  controllers.SetUserTimezoneController,
-  controllers.ResetUserPasswordController,
-  controllers.UpdateUserPasswordController,
-
-  controllers.CreateOrgController,
-  controllers.GetOrgsListController,
-  controllers.GetOrgController,
-  controllers.IsOrgExistController,
-  controllers.SetOrgInfoController,
-  controllers.SetOrgOwnerController,
-  controllers.DeleteOrgController,
-  controllers.DeleteUserController,
-
-  controllers.CreateProjectController,
-  controllers.GetProjectController,
-  controllers.GetProjectsListController,
-  controllers.IsProjectExistController,
-  controllers.DeleteProjectController,
-  controllers.SetProjectInfoController,
-
-  controllers.GetMembersController,
-  controllers.CreateMemberController,
-  controllers.EditMemberController,
-  controllers.DeleteMemberController,
-
-  controllers.GetOrgUsersController,
-
-  controllers.GetConnectionsController,
-  controllers.CreateConnectionController,
-  controllers.EditConnectionController,
-  controllers.DeleteConnectionController,
-
-  controllers.GetBranchesListController,
-  controllers.IsBranchExistController,
   controllers.CreateBranchController,
   controllers.DeleteBranchController,
-
-  controllers.GetRepoController,
-  controllers.CommitRepoController,
-  controllers.PushRepoController,
-  controllers.PullRepoController,
-  controllers.RevertRepoToLastCommitController,
-  controllers.RevertRepoToProductionController,
-  controllers.MergeRepoController,
+  controllers.GetBranchesListController,
+  controllers.IsBranchExistController,
 
   controllers.MoveCatalogNodeController,
   controllers.RenameCatalogNodeController,
 
+  controllers.CreateConnectionController,
+  controllers.DeleteConnectionController,
+  controllers.EditConnectionController,
+  controllers.GetConnectionsController,
+
+  controllers.CreateDashboardController,
+  controllers.CreateTempDashboardController,
+  controllers.DeleteDashboardController,
+  controllers.GetDashboardController,
+  controllers.GetDashboardsListController,
+  controllers.ModifyDashboardController,
+
+  controllers.CreateFileController,
+  controllers.DeleteFileController,
+  controllers.GetFileController,
+  controllers.SaveFileController,
+
   controllers.CreateFolderController,
   controllers.DeleteFolderController,
 
-  controllers.GetFileController,
-  controllers.DeleteFileController,
-  controllers.CreateFileController,
-  controllers.SaveFileController,
-
-  controllers.GetModelsListController,
-  controllers.GetModelController,
-
-  controllers.GetViewsController,
-
-  controllers.GetDashboardsListController,
-  controllers.GetDashboardController,
-  controllers.CreateTempDashboardController,
-  controllers.CreateDashboardController,
-  controllers.ModifyDashboardController,
-  controllers.DeleteDashboardController,
-
-  controllers.GetVizsController,
-  controllers.CreateVizController,
-  controllers.ModifyVizController,
-  controllers.DeleteVizController,
-
-  controllers.GetMconfigController,
   controllers.CreateTempMconfigController,
   controllers.CreateTempMconfigAndQueryController,
-  //
+  controllers.GetMconfigController,
+
+  controllers.CreateMemberController,
+  controllers.DeleteMemberController,
+  controllers.EditMemberController,
+  controllers.GetMembersController,
+
+  controllers.GetModelController,
+  controllers.GetModelsListController,
+
+  controllers.GetNavController,
+
+  controllers.GetOrgUsersController,
+
+  controllers.CreateOrgController,
+  controllers.DeleteOrgController,
+  controllers.GetOrgController,
+  controllers.GetOrgsListController,
+  controllers.IsOrgExistController,
+  controllers.SetOrgInfoController,
+  controllers.SetOrgOwnerController,
+
+  controllers.CreateProjectController,
+  controllers.DeleteProjectController,
+  controllers.GetProjectController,
+  controllers.GetProjectsListController,
+  controllers.IsProjectExistController,
+  controllers.SetProjectInfoController,
+
+  controllers.CancelQueriesController,
   controllers.GetQueryController,
   controllers.RunQueriesController,
   controllers.RunQueriesDryController,
-  controllers.CancelQueriesController,
-  //
-  controllers.GetNavController,
-  //
-  controllers.GetAvatarBigController,
-  controllers.SetAvatarController
+
+  controllers.DeleteRecordsController,
+  controllers.SeedRecordsController,
+
+  controllers.CommitRepoController,
+  controllers.GetRepoController,
+  controllers.MergeRepoController,
+  controllers.PullRepoController,
+  controllers.PushRepoController,
+  controllers.RevertRepoToLastCommitController,
+  controllers.RevertRepoToProductionController,
+
+  controllers.RebuildStructSpecialController,
+
+  controllers.ConfirmUserEmailController,
+  controllers.DeleteUserController,
+  controllers.GetUserProfileController,
+  controllers.LoginUserController,
+  controllers.LogoutUserController,
+  controllers.RegisterUserController,
+  controllers.ResetUserPasswordController,
+  controllers.SetUserNameController,
+  controllers.SetUserTimezoneController,
+  controllers.UpdateUserPasswordController,
+
+  controllers.GetViewsController,
+
+  controllers.CreateVizController,
+  controllers.DeleteVizController,
+  controllers.GetVizsController,
+  controllers.ModifyVizController
 ];
