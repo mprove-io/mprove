2c21fc3dbaad8f66201b711ab18c26cbfe58cefa
akalitenya <akalitenya@mprove.io>
2021-04-02 17:20:29 +0500

    front login p2

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,35 @@ module.exports = {
   purge: ['apps/front/src/**/*.{html,ts}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      boxShadow: {
+        m1: '0px 1px 4px rgba(21,34,50,0.078)'
+      }
+    },
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      b1: '#F2F5FA',
+      b2: '#365EE0'
+    }),
+    textColor: theme => ({
+      ...theme('colors'),
+      t1: '#00030B',
+      t2: '#465EDC'
+    }),
+    borderColor: theme => ({
+      ...theme('colors'),
+      r1: '#D7DBEC',
+      r2: '#365EE0'
+    })
   },
   variants: {
     extend: {}
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/forms'),
+    require('@tailwindcss/forms')({
+      strategy: 'class'
+    }),
     require('@tailwindcss/line-clamp'),
     require('@tailwindcss/typography')
   ]
