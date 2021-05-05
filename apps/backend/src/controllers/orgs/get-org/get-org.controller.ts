3962a30ae17d6abe8b4d27cd33448a941dfd34fb
akalitenya <akalitenya@mprove.io>
2021-05-05 11:58:36 +0500

    backend get org

--- apps/backend/src/controllers/orgs/get-org/get-org.controller.ts
+++ apps/backend/src/controllers/orgs/get-org/get-org.controller.ts
@@ -1,13 +1,23 @@
 import { Controller, Post } from '@nestjs/common';
+import { In } from 'typeorm';
 import { apiToBackend } from '~backend/barrels/api-to-backend';
+import { common } from '~backend/barrels/common';
 import { entities } from '~backend/barrels/entities';
 import { wrapper } from '~backend/barrels/wrapper';
 import { AttachUser, ValidateRequest } from '~backend/decorators/_index';
+import {
+  MembersRepository,
+  ProjectsRepository
+} from '~backend/models/store-repositories/_index';
 import { OrgsService } from '~backend/services/orgs.service';
 
 @Controller()
 export class GetOrgController {
-  constructor(private orgsService: OrgsService) {}
+  constructor(
+    private orgsService: OrgsService,
+    private membersRepository: MembersRepository,
+    private projectsRepository: ProjectsRepository
+  ) {}
 
   @Post(apiToBackend.ToBackendRequestInfoNameEnum.ToBackendGetOrg)
   async getOrg(
@@ -19,10 +29,28 @@ export class GetOrgController {
 
     let org = await this.orgsService.getOrgCheckExists({ orgId: orgId });
 
-    await this.orgsService.checkUserIsOrgOwner({
-      org: org,
-      userId: user.user_id
-    });
+    if (org.owner_id !== user.user_id) {
+      let userMembers = await this.membersRepository.find({
+        member_id: user.user_id
+      });
+
+      let projectIds = userMembers.map(m => m.project_id);
+      let projects =
+        projectIds.length === 0
+          ? []
+          : await this.projectsRepository.find({
+              project_id: In(projectIds),
+              org_id: orgId
+            });
+
+      let orgIds = projects.map(x => x.org_id);
+
+      if (orgIds.indexOf(orgId) < 0) {
+        throw new common.ServerError({
+          message: apiToBackend.ErEnum.BACKEND_FORBIDDEN_ORG
+        });
+      }
+    }
 
     let payload: apiToBackend.ToBackendGetOrgResponsePayload = {
       org: wrapper.wrapToApiOrg(org)
