4de60df1045c63eced95061481519836056fa4d0
akalitenya <akalitenya@mprove.io>
2021-06-16 14:58:21 +0500

    front model p27

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,20 @@ var blue1 = '#F2F5FA';
 var blue2 = '#E7F2FD';
 var blue3 = '#365EE0';
 var blue4 = '#465EDC';
+var blue5 = '#C3DCF3';
+var blue6 = '#C3DCF372';
 
 var gray1 = '#4D4F5C';
 var gray2 = '#00030B';
 var gray3 = '#D7DBEC';
 var gray4 = '#D7DBECA3';
 var gray5 = '#4F505C';
+
+var green1 = '#B6E1BC';
+var green2 = '#E9F6EB';
+
+var purple1 = '#90A6EE';
+var purple2 = '#E1E7FB';
 
 var red1 = '#DE4343';
 
@@ -32,7 +40,13 @@ module.exports = {
       blue1,
       blue2,
       blue3,
-      red1
+      red1,
+      green1,
+      green2,
+      blue5,
+      blue6,
+      purple1,
+      purple2
     }),
     textColor: theme => ({
       ...theme('colors'),
