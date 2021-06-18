5e992ba3045a7c25b1d9a4701d5e243dad009822
akalitenya <akalitenya@mprove.io>
2021-06-18 17:33:04 +0500

    front model p34

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
   purge: ['apps/front/src/**/*.{html,ts}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    groupLevel: 10,
     groupScope: 'scope',
     extend: {
       scale: {
