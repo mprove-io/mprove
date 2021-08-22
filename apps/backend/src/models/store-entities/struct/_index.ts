5d156a40a772b7564ff66bd217dcd2a336c99be0
akalitenya <akalitenya@mprove.io>
2021-08-22 14:09:43 +0500

    backend migrations

--- apps/backend/src/models/store-entities/struct/_index.ts
+++
@@ -1,6 +0,0 @@
-export * from './dashboard.entity';
-export * from './mconfig.entity';
-export * from './model.entity';
-export * from './query.entity';
-export * from './struct.entity';
-export * from './viz.entity';
