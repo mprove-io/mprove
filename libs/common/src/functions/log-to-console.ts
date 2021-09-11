6cad07c5e93dc1f1a806ca847f0dbae4a56ee4c7
akalitenya <akalitenya@mprove.io>
2021-09-11 19:09:29 +0500

    logs json

--- libs/common/src/functions/log-to-console.ts
+++ libs/common/src/functions/log-to-console.ts
@@ -2,15 +2,14 @@ import { ConfigService } from '@nestjs/config';
 import * as util from 'util';
 import { enums } from '~common/barrels/enums';
 import { Config } from '~common/interfaces/_index';
+import { enumToBoolean } from './enum-to-boolean';
 
 export function logToConsole(object: any, cs?: ConfigService<Config>) {
   let mproveLogIsColor =
     cs?.get<Config['mproveLogIsColor']>('mproveLogIsColor') ||
     enums.BoolEnum.TRUE;
 
-  if (mproveLogIsColor === enums.BoolEnum.TRUE) {
-    console.log(util.inspect(object, false, null, true));
-  } else {
-    console.log(JSON.stringify(util.inspect(object, false, null, false)));
-  }
+  let isColor: boolean = enumToBoolean(mproveLogIsColor);
+
+  console.log(util.inspect(object, false, null, isColor));
 }
