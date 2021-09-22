131baeb2bfd21dba0c883313c0d40bca1411e5aa
akalitenya <akalitenya@mprove.io>
2021-09-22 12:06:21 +0500

    navigate from logo to prod master vizs

--- apps/front/src/app/modules/shared/logo/logo.component.ts
+++ apps/front/src/app/modules/shared/logo/logo.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { NavigateService } from '~front/app/services/navigate.service';
 
 @Component({
   selector: 'm-logo',
   templateUrl: 'logo.component.html'
 })
 export class LogoComponent {
-  constructor() {}
+  constructor(public navigateService: NavigateService) {}
+
+  goToVizs() {
+    this.navigateService.navigateToProdMasterVizs();
+  }
 }
--- apps/front/src/app/services/navigate.service.ts
+++ apps/front/src/app/services/navigate.service.ts
@@ -251,6 +251,22 @@ export class NavigateService {
     ]);
   }
 
+  navigateToProdMasterVizs() {
+    let navTo = [
+      common.PATH_ORG,
+      this.nav.orgId,
+      common.PATH_PROJECT,
+      this.nav.projectId,
+      common.PATH_REPO,
+      common.PROD_REPO_ID,
+      common.PATH_BRANCH,
+      common.BRANCH_MASTER,
+      common.PATH_VISUALIZATIONS
+    ];
+
+    this.router.navigate(navTo);
+  }
+
   navigateToVizs(item?: { navParts?: string[]; extra?: any }) {
     let { navParts, extra } = item;
 
