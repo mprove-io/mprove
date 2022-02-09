924f95fbea7f7c2da8b864e5d39b27122e629c8c
akalitenya <akalitenya@mprove.io>
2022-02-09 16:31:04 +0500

    menus p7

--- apps/front/src/app/modules/model/chart-options/chart-options.component.ts
+++ apps/front/src/app/modules/model/chart-options/chart-options.component.ts
@@ -1,5 +1,4 @@
-import { ChangeDetectorRef, Component, Input, OnDestroy } from '@angular/core';
-import { tap } from 'rxjs/operators';
+import { ChangeDetectorRef, Component, Input } from '@angular/core';
 import { UiQuery } from '~front/app/queries/ui.query';
 import { ApiService } from '~front/app/services/api.service';
 import { MyDialogService } from '~front/app/services/my-dialog.service';
@@ -11,21 +10,12 @@ import { common } from '~front/barrels/common';
   selector: 'm-chart-options',
   templateUrl: './chart-options.component.html'
 })
-export class ChartOptionsComponent implements OnDestroy {
+export class ChartOptionsComponent {
   @Input()
   isDisabled: boolean;
 
   @Input()
   mconfig: common.MconfigX;
-
-  menuId = 'chartOptions';
-
-  openedMenuId: string;
-  openedMenuId$ = this.uiQuery.openedMenuId$.pipe(
-    tap(x => (this.openedMenuId = x))
-  );
-
-  isChartOptionsMenuOpen = false;
 
   constructor(
     public uiQuery: UiQuery,
@@ -36,40 +26,12 @@ export class ChartOptionsComponent implements OnDestroy {
     private cd: ChangeDetectorRef
   ) {}
 
-  openMenu() {
-    this.isChartOptionsMenuOpen = true;
-    this.uiStore.update({ openedMenuId: this.menuId });
-  }
-
-  closeMenu(event?: MouseEvent) {
-    if (common.isDefined(event)) {
-      event.stopPropagation();
-    }
-    this.isChartOptionsMenuOpen = false;
-    this.uiStore.update({ openedMenuId: undefined });
-  }
-
-  toggleMenu(event?: MouseEvent) {
-    event.stopPropagation();
-    if (this.isChartOptionsMenuOpen === true) {
-      this.closeMenu();
-    } else {
-      this.openMenu();
-    }
-  }
-
   viewBlockML(event?: MouseEvent) {
     event.stopPropagation();
-    this.closeMenu();
 
     this.myDialogService.showViewBlockml({
       apiService: this.apiService,
       mconfig: this.mconfig
     });
-  }
-
-  ngOnDestroy() {
-    if (this.menuId === this.openedMenuId)
-      this.uiStore.update({ openedMenuId: undefined });
   }
 }
