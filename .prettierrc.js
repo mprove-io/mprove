9742715a6a33b4c0f6494cef1eedfa7ede2acde4
akalitenya <akalitenya@mprove.io>
2022-02-12 19:52:16 +0500

    tailwind prettier p2

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'strict',
+  // jsxBracketSameLine: false,
+  // jsxSingleQuote: false,
+  // parser: babylon,
+  printWidth: 80,
+  proseWrap: 'preserve',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false,
+  plugins: [require('./scripts/merged-prettier-plugin.js')]
+};
