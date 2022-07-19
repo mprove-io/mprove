0d302e7344689c349707bb49d974f60045c6e854
akalitenya <akalitenya@mprove.io>
2022-07-19 14:39:25 +0000

    config p6

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo chown node /etc/hosts 
+# sudo chown node /etc/hosts 
 
 echo "$(echo '127.0.0.1 db' | cat - /etc/hosts)" > /etc/hosts 
 echo "$(echo '127.0.0.1 rabbit' | cat - /etc/hosts)" > /etc/hosts 
