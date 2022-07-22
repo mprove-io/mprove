d9356703ccca431fac401018724fec88921d7761
akalitenya <akalitenya@mprove.io>
2022-07-22 04:49:59 +0000

    cypress

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 containerWorkspaceFolder=$1
+cp -n $containerWorkspaceFolder/.devcontainer/.zshrc.example $containerWorkspaceFolder/.devcontainer/.zshrc 
+ln -sf $containerWorkspaceFolder/.devcontainer/.zshrc ~/.zshrc 
 
 # sudo chown node node_modules 
 # sudo chown node dist 
 # sudo chown node /home/node/mprove_data/organizations
 # sudo chown node /home/node/mprove_data/blockml-logs
 
-cp -n $containerWorkspaceFolder/.devcontainer/.zshrc.example $containerWorkspaceFolder/.devcontainer/.zshrc 
-ln -sf $containerWorkspaceFolder/.devcontainer/.zshrc ~/.zshrc 
+
 
 
--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+containerWorkspaceFolder=$1
+$containerWorkspaceFolder/node_modules/.bin/cypress install
+
 # sudo chown node /etc/hosts 
 
 echo "$(echo '127.0.0.1 db' | cat - /etc/hosts)" > /etc/hosts 
-echo "$(echo '127.0.0.1 rabbit' | cat - /etc/hosts)" > /etc/hosts 
+echo "$(echo '127.0.0.1 rabbit' | cat - /etc/hosts)" > /etc/hosts  
