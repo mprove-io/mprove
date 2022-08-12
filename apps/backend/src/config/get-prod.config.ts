364a1de79d1c8dbde5cef1002e474cc6f5c0a94f
akalitenya <akalitenya@mprove.io>
2022-08-12 10:25:37 +0000

    turn off test routes for prod

--- apps/backend/src/config/get-prod.config.ts
+++ apps/backend/src/config/get-prod.config.ts
@@ -1,8 +1,10 @@
+import { common } from '~backend/barrels/common';
 import { interfaces } from '~backend/barrels/interfaces';
 
 export function getProdConfig(devConfig: interfaces.Config) {
   let prodConfig = Object.assign({}, devConfig);
 
+  prodConfig.allowTestRoutes = common.BoolEnum.FALSE;
   // testConfig.backendLogIsColor = common.BoolEnum.FALSE;
 
   return prodConfig;
