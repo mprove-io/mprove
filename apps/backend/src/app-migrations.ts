c72bb997bb74f900668e8c35bb9292c9f7d4a151
akalitenya <akalitenya@mprove.io>
2022-10-04 10:58:44 +0000

    env vars p21

--- apps/backend/src/app-migrations.ts
+++ apps/backend/src/app-migrations.ts
@@ -1,33 +1,3 @@
-import { All1629629416532 } from './migrations/1629629416532-All';
-import { All1631622949794 } from './migrations/1631622949794-All';
-import { All1639645850004 } from './migrations/1639645850004-All';
-import { All1641546031332 } from './migrations/1641546031332-All';
-import { All1641972091019 } from './migrations/1641972091019-All';
-import { All1643013591077 } from './migrations/1643013591077-All';
-import { All1643368584504 } from './migrations/1643368584504-All';
-import { All1662289276856 } from './migrations/1662289276856-All';
-import { All1662373302974 } from './migrations/1662373302974-All';
-import { All1662459060915 } from './migrations/1662459060915-All';
-import { All1663489919330 } from './migrations/1663489919330-All';
-import { All1663757506953 } from './migrations/1663757506953-All';
-import { All1664003339398 } from './migrations/1664003339398-All';
-import { All1664369364119 } from './migrations/1664369364119-All';
-import { All1664716139521 } from './migrations/1664716139521-All';
+import { All1664880925471 } from './migrations/1664880925471-All';
 
-export const appMigrations = [
-  All1629629416532,
-  All1631622949794,
-  All1639645850004,
-  All1641546031332,
-  All1641972091019,
-  All1643013591077,
-  All1643368584504,
-  All1662289276856,
-  All1662373302974,
-  All1662459060915,
-  All1663489919330,
-  All1663757506953,
-  All1664003339398,
-  All1664369364119,
-  All1664716139521
-];
+export const appMigrations = [All1664880925471];
