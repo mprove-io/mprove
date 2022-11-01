512251bbc25a1b0c1335b86faf65f388c88689a1
akalitenya <akalitenya@mprove.io>
2022-11-01 13:21:05 +0000

    spinner

--- apps/front/src/app/constants/top.ts
+++ apps/front/src/app/constants/top.ts
@@ -2,7 +2,7 @@ import { common } from '~front/barrels/common';
 
 export const SPECIAL_ERROR = 'SPECIAL_ERROR';
 export const PASSWORD_RESET_EMAIL = 'PASSWORD_RESET_EMAIL';
-export const MIN_TIME_TO_SPIN = 1000;
+export const MIN_TIME_TO_SPIN = 0;
 
 export const LOCAL_STORAGE_TOKEN = 'token';
 export const LOCAL_STORAGE_ORG_ID = 'orgId';
