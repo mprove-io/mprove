1f2684ac19ccad512b79e5568b256bcb9a513074
akalitenya <akalitenya@mprove.io>
2022-11-12 16:46:45 +0000

    logs p4

--- apps/disk/src/app.module.ts
+++ apps/disk/src/app.module.ts
@@ -1,6 +1,7 @@
 import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 import { appControllers } from './app-controllers';
 import { appServices } from './app-services';
 import { common } from './barrels/common';
@@ -14,23 +15,38 @@ import { getConfig } from './config/get.config';
       isGlobal: true
     }),
 
+    LoggerModule.forRoot({
+      pinoHttp: {
+        // level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+        transport:
+          process.env.DISK_LOG_IS_STRINGIFY === common.BoolEnum.FALSE
+            ? {
+                target: 'pino-pretty',
+                options: {
+                  colorize: true,
+                  ignore: 'context,pid,hostname',
+                  levelFirst: false,
+                  translateTime: 'UTC:yyyy-mm-dd HH:MM:ss',
+                  messageFormat:
+                    '   \x1B[33m[{context}]\x1b[0m' + ' \x1B[32m{msg}\x1B[39m'
+                }
+              }
+            : undefined
+      }
+    }),
+
     RabbitMQModule.forRootAsync(RabbitMQModule, {
       useFactory: (cs: ConfigService<interfaces.Config>) => {
-        let rabbitUser = cs.get<interfaces.Config['diskRabbitUser']>(
-          'diskRabbitUser'
-        );
-        let rabbitPass = cs.get<interfaces.Config['diskRabbitPass']>(
-          'diskRabbitPass'
-        );
-        let rabbitHost = cs.get<interfaces.Config['diskRabbitHost']>(
-          'diskRabbitHost'
-        );
-        let rabbitPort = cs.get<interfaces.Config['diskRabbitPort']>(
-          'diskRabbitPort'
-        );
-        let rabbitProtocol = cs.get<interfaces.Config['diskRabbitProtocol']>(
-          'diskRabbitProtocol'
-        );
+        let rabbitUser =
+          cs.get<interfaces.Config['diskRabbitUser']>('diskRabbitUser');
+        let rabbitPass =
+          cs.get<interfaces.Config['diskRabbitPass']>('diskRabbitPass');
+        let rabbitHost =
+          cs.get<interfaces.Config['diskRabbitHost']>('diskRabbitHost');
+        let rabbitPort =
+          cs.get<interfaces.Config['diskRabbitPort']>('diskRabbitPort');
+        let rabbitProtocol =
+          cs.get<interfaces.Config['diskRabbitProtocol']>('diskRabbitProtocol');
 
         return {
           exchanges: [
@@ -58,4 +74,15 @@ import { getConfig } from './config/get.config';
   controllers: appControllers,
   providers: appServices
 })
-export class AppModule {}
+// implements OnModuleInit
+export class AppModule {
+  //   constructor(private pinoLogger: PinoLogger) {}
+  //   async onModuleInit() {
+  //     console.log(123);
+  //     this.pinoLogger.info(12345);
+  //     this.pinoLogger.error(555);
+  //     this.pinoLogger.error(['a', 'b']);
+  //     this.pinoLogger.info(['c', 'd']);
+  //     this.pinoLogger.error(new ServerError({ message: '555' }));
+  //   }
+}
--- apps/disk/src/main.ts
+++ apps/disk/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import 'module-alias/register';
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 import { listenProcessEventsDisk } from './functions/listen-process-events-disk';
 import { logToConsoleDisk } from './functions/log-to-console-disk';
@@ -9,7 +10,10 @@ async function bootstrap() {
 
   listenProcessEventsDisk();
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true
+  });
+  app.useLogger(app.get(Logger));
 
   await app.listen(process.env.LISTEN_PORT || 3002);
 }
