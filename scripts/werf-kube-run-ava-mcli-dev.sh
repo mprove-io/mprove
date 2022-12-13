5f623e843394a85ab1643ae7cb13b9261a10ebfc
akalitenya <akalitenya@mprove.io>
2022-12-13 21:03:18 +0000

    cli p61

--- scripts/werf-kube-run-ava-mcli-dev.sh
+++ scripts/werf-kube-run-ava-mcli-dev.sh
@@ -14,9 +14,33 @@ werf kube-run --repo ghcr.io/mprove-io/mprove --overrides='{
               "name": "mcli-envs"
             }
           }
-        ]
+        ],
+        "volumeMounts": [
+          {
+            "name": "secrets-volume",
+            "mountPath": "/usr/src/app/secrets"
+          }
+        ]        
       }
-    ]    
+    ],
+    "volumes": [
+      {
+        "name": "secrets-volume",
+        "secret": {
+          "secretName": "backend-common-secret-files",
+          "items": [
+            {
+              "key": "contentRemotePrivateKey",
+              "path": "first-project-remote-private-key.pem"
+            },
+            {
+              "key": "contentRemotePublicKey",
+              "path": "first-project-remote-public-key.pem"
+            }
+          ]          
+        }
+      }
+    ]
   }
 }' \
   mcli --dev -- yarn ava:mcli
