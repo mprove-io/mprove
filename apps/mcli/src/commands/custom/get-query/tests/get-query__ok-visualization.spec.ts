b740a9f2e0608c8c0d1ea32afd59950507ae2510
akalitenya <akalitenya@mprove.io>
2022-12-16 18:25:10 +0000

    cli p77

---
+++ apps/mcli/src/commands/custom/get-query/tests/get-query__ok-visualization.spec.ts
@@ -0,0 +1,119 @@
+import test from 'ava';
+import { common } from '~mcli/barrels/common';
+import { getConfig } from '~mcli/config/get.config';
+import { logToConsoleMcli } from '~mcli/functions/log-to-console-mcli';
+import { prepareTest } from '~mcli/functions/prepare-test';
+import { CustomContext } from '~mcli/models/custom-command';
+import { GetQueryCommand } from '../get-query';
+
+let testId = 'mcli__get-query__ok-visualization';
+
+test('1', async t => {
+  let context: CustomContext;
+  let code: number;
+
+  let defaultBranch = common.BRANCH_MASTER;
+
+  let projectId = common.makeId();
+  let commandLine = `get-query -p ${projectId} --repo dev --branch ${defaultBranch} \
+--env prod --visualization-id 4K9SNSMG0IQPQZ9CL23U --get-sql`;
+
+  let userId = common.makeId();
+  let email = `${testId}@example.com`;
+  let password = '123123';
+
+  let orgId = 't' + testId;
+  let orgName = testId;
+
+  let projectName = testId;
+
+  let config = getConfig();
+
+  try {
+    let { cli, mockContext } = await prepareTest({
+      command: GetQueryCommand,
+      config: config,
+      deletePack: {
+        emails: [email],
+        orgIds: [orgId],
+        projectIds: [projectId],
+        projectNames: [projectName]
+      },
+      seedPack: {
+        users: [
+          {
+            userId,
+            email: email,
+            password: password,
+            isEmailVerified: common.BoolEnum.TRUE
+          }
+        ],
+        orgs: [
+          {
+            orgId: orgId,
+            ownerEmail: email,
+            name: orgName
+          }
+        ],
+        projects: [
+          {
+            orgId,
+            projectId,
+            name: projectName,
+            testProjectId: 'first-project',
+            defaultBranch: defaultBranch,
+            remoteType: common.ProjectRemoteTypeEnum.Managed,
+            gitUrl: undefined,
+            publicKey: undefined,
+            privateKey: undefined
+          }
+        ],
+        members: [
+          {
+            memberId: userId,
+            email,
+            projectId,
+            isAdmin: common.BoolEnum.TRUE,
+            isEditor: common.BoolEnum.TRUE,
+            isExplorer: common.BoolEnum.TRUE
+          }
+        ],
+        connections: [
+          {
+            projectId: projectId,
+            connectionId: 'c1_postgres',
+            envId: common.PROJECT_ENV_PROD,
+            type: common.ConnectionTypeEnum.PostgreSQL,
+            host: '0.0.0.0',
+            port: 5432,
+            database: 'p_db',
+            username: 'p_user',
+            password: 'p_pass'
+          }
+        ]
+      },
+      loginEmail: email,
+      loginPassword: password
+    });
+
+    context = mockContext as any;
+    code = await cli.run([...commandLine.split(' ')], context);
+  } catch (e) {
+    logToConsoleMcli({
+      log: e,
+      logLevel: common.LogLevelEnum.Error,
+      context: context,
+      isJson: true
+    });
+  }
+
+  let isPass = code === 0 && context.stdout.toString().includes('query');
+
+  if (isPass === false) {
+    console.log(context.stdout.toString());
+    console.log(context.stderr.toString());
+  }
+
+  t.is(code, 0);
+  t.is(context.stdout.toString().includes('query'), true);
+});
