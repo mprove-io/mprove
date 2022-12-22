82975b69ea5e2554471a4cbdb012e985473d4c16
akalitenya <akalitenya@mprove.io>
2022-12-22 15:59:07 +0000

    cli p144

--- apps/mcli/src/commands/custom/sync/tests/first-sync/f__local-modified__dev-deleted__modified-local.spec.ts
+++ apps/mcli/src/commands/custom/sync/tests/first-sync/f__local-modified__dev-deleted__modified-local.spec.ts
@@ -2,171 +2,186 @@ import test from 'ava';
 import * as fse from 'fs-extra';
 import { apiToBackend } from '~mcli/barrels/api-to-backend';
 import { common } from '~mcli/barrels/common';
+import { constants } from '~mcli/barrels/constants';
 import { getConfig } from '~mcli/config/get.config';
+import { checkIsTrue } from '~mcli/functions/check-is-true';
 import { cloneRepo } from '~mcli/functions/clone-repo';
 import { logToConsoleMcli } from '~mcli/functions/log-to-console-mcli';
 import { mreq } from '~mcli/functions/mreq';
 import { prepareTest } from '~mcli/functions/prepare-test';
 import { CustomContext } from '~mcli/models/custom-command';
 import { SyncCommand } from '../../sync';
+let retry = require('async-retry');
 
 let testId = 'mcli_f__local-modified__dev-deleted__modified-local';
 
 test('1', async t => {
-  let context: CustomContext;
   let code: number;
-  let config = getConfig();
+  let isPass: boolean;
+  let parsedOutput: any;
+  let context: CustomContext;
 
-  let defaultBranch = common.BRANCH_MAIN;
-  let env = common.PROJECT_ENV_PROD;
+  let fileName = 'README.md';
 
-  let repoPath = `${config.mproveCliTestReposPath}/${testId}`;
+  await retry(async (bail: any) => {
+    let config = getConfig();
 
-  await cloneRepo({
-    repoPath: repoPath,
-    gitUrl: config.mproveCliTestLocalSourceGitUrl
-  });
+    let defaultBranch = common.BRANCH_MAIN;
+    let env = common.PROJECT_ENV_PROD;
 
-  let projectId = common.makeId();
+    let repoPath = `${config.mproveCliTestReposPath}/${testId}`;
 
-  let commandLine = `sync \
+    await cloneRepo({
+      repoPath: repoPath,
+      gitUrl: config.mproveCliTestLocalSourceGitUrl
+    });
+
+    let projectId = common.makeId();
+
+    let commandLine = `sync \
 -p ${projectId} \
 --env ${env} \
 --local-path ${repoPath} \
 --json \
 --debug`;
 
-  let userId = common.makeId();
-  let email = `${testId}@example.com`;
-  let password = '123123';
+    let userId = common.makeId();
+    let email = `${testId}@example.com`;
+    let password = '123123';
 
-  let orgId = 't' + testId;
-  let orgName = testId;
+    let orgId = 't' + testId;
+    let orgName = testId;
 
-  let projectName = testId;
+    let projectName = testId;
 
-  let fileName = 'README.md';
+    try {
+      let { cli, mockContext } = await prepareTest({
+        command: SyncCommand,
+        config: config,
+        deletePack: {
+          emails: [email],
+          orgIds: [orgId],
+          projectIds: [projectId],
+          projectNames: [projectName]
+        },
+        seedPack: {
+          users: [
+            {
+              userId,
+              email: email,
+              password: password,
+              isEmailVerified: common.BoolEnum.TRUE
+            }
+          ],
+          orgs: [
+            {
+              orgId: orgId,
+              ownerEmail: email,
+              name: orgName
+            }
+          ],
+          projects: [
+            {
+              orgId,
+              projectId,
+              name: projectName,
+              defaultBranch: common.BRANCH_MAIN,
+              remoteType: common.ProjectRemoteTypeEnum.GitClone,
+              gitUrl: config.mproveCliTestDevSourceGitUrl,
+              publicKey: fse
+                .readFileSync(config.mproveCliTestPublicKeyPath)
+                .toString(),
+              privateKey: fse
+                .readFileSync(config.mproveCliTestPrivateKeyPath)
+                .toString()
+            }
+          ],
+          members: [
+            {
+              memberId: userId,
+              email,
+              projectId,
+              isAdmin: common.BoolEnum.TRUE,
+              isEditor: common.BoolEnum.TRUE,
+              isExplorer: common.BoolEnum.TRUE
+            }
+          ],
+          connections: [
+            {
+              projectId: projectId,
+              connectionId: 'c1_postgres',
+              envId: common.PROJECT_ENV_PROD,
+              type: common.ConnectionTypeEnum.PostgreSQL,
+              host: 'dwh-postgres',
+              port: 5432,
+              database: 'p_db',
+              username: 'postgres',
+              password: config.mproveCliTestDwhPostgresPassword
+            }
+          ]
+        },
+        loginEmail: email,
+        loginPassword: password
+      });
 
-  try {
-    let { cli, mockContext } = await prepareTest({
-      command: SyncCommand,
-      config: config,
-      deletePack: {
-        emails: [email],
-        orgIds: [orgId],
-        projectIds: [projectId],
-        projectNames: [projectName]
-      },
-      seedPack: {
-        users: [
-          {
-            userId,
-            email: email,
-            password: password,
-            isEmailVerified: common.BoolEnum.TRUE
-          }
-        ],
-        orgs: [
-          {
-            orgId: orgId,
-            ownerEmail: email,
-            name: orgName
-          }
-        ],
-        projects: [
-          {
-            orgId,
-            projectId,
-            name: projectName,
-            defaultBranch: common.BRANCH_MAIN,
-            remoteType: common.ProjectRemoteTypeEnum.GitClone,
-            gitUrl: config.mproveCliTestDevSourceGitUrl,
-            publicKey: fse
-              .readFileSync(config.mproveCliTestPublicKeyPath)
-              .toString(),
-            privateKey: fse
-              .readFileSync(config.mproveCliTestPrivateKeyPath)
-              .toString()
-          }
-        ],
-        members: [
-          {
-            memberId: userId,
-            email,
-            projectId,
-            isAdmin: common.BoolEnum.TRUE,
-            isEditor: common.BoolEnum.TRUE,
-            isExplorer: common.BoolEnum.TRUE
-          }
-        ],
-        connections: [
-          {
-            projectId: projectId,
-            connectionId: 'c1_postgres',
-            envId: common.PROJECT_ENV_PROD,
-            type: common.ConnectionTypeEnum.PostgreSQL,
-            host: 'dwh-postgres',
-            port: 5432,
-            database: 'p_db',
-            username: 'postgres',
-            password: config.mproveCliTestDwhPostgresPassword
-          }
-        ]
-      },
-      loginEmail: email,
-      loginPassword: password
-    });
+      context = mockContext as any;
 
-    context = mockContext as any;
+      let filePath = `${repoPath}/${fileName}`;
 
-    let filePath = `${repoPath}/${fileName}`;
+      await fse.writeFile(filePath, '1');
 
-    await fse.writeFile(filePath, '1');
+      let deleteFileReqPayload: apiToBackend.ToBackendDeleteFileRequestPayload =
+        {
+          projectId: projectId,
+          branchId: defaultBranch,
+          envId: env,
+          fileNodeId: `${projectId}/${fileName}`
+        };
 
-    let deleteFileReqPayload: apiToBackend.ToBackendDeleteFileRequestPayload = {
-      projectId: projectId,
-      branchId: defaultBranch,
-      envId: env,
-      fileNodeId: `${projectId}/${fileName}`
-    };
+      await mreq<apiToBackend.ToBackendDeleteFileResponse>({
+        loginToken: context.loginToken,
+        pathInfoName:
+          apiToBackend.ToBackendRequestInfoNameEnum.ToBackendDeleteFile,
+        payload: deleteFileReqPayload,
+        host: context.config.mproveCliHost
+      });
 
-    await mreq<apiToBackend.ToBackendDeleteFileResponse>({
-      loginToken: context.loginToken,
-      pathInfoName:
-        apiToBackend.ToBackendRequestInfoNameEnum.ToBackendDeleteFile,
-      payload: deleteFileReqPayload,
-      host: context.config.mproveCliHost
-    });
+      code = await cli.run(commandLine.split(' '), context);
+    } catch (e) {
+      logToConsoleMcli({
+        log: e,
+        logLevel: common.LogLevelEnum.Error,
+        context: context,
+        isJson: true
+      });
+    }
 
-    code = await cli.run(commandLine.split(' '), context);
-  } catch (e) {
+    try {
+      parsedOutput = JSON.parse(context.stdout.toString());
+    } catch (e) {
+      logToConsoleMcli({
+        log: e,
+        logLevel: common.LogLevelEnum.Error,
+        context: context,
+        isJson: true
+      });
+    }
+
+    isPass = checkIsTrue(
+      code === 0 &&
+        parsedOutput.debug.localChangesToCommit.length === 1 &&
+        parsedOutput.debug.localChangesToCommit[0].fileName === fileName &&
+        parsedOutput.debug.localChangesToCommit[0].status ===
+          common.FileStatusEnum.Modified
+    );
+  }, constants.RETRY_OPTIONS).catch((er: any) => {
     logToConsoleMcli({
-      log: e,
+      log: er,
       logLevel: common.LogLevelEnum.Error,
-      context: context,
-      isJson: true
+      context: undefined,
+      isJson: false
     });
-  }
-
-  let parsedOutput: any;
-
-  try {
-    parsedOutput = JSON.parse(context.stdout.toString());
-  } catch (e) {
-    logToConsoleMcli({
-      log: e,
-      logLevel: common.LogLevelEnum.Error,
-      context: context,
-      isJson: true
-    });
-  }
-
-  let isPass =
-    code === 0 &&
-    parsedOutput.debug.localChangesToCommit.length === 1 &&
-    parsedOutput.debug.localChangesToCommit[0].fileName === fileName &&
-    parsedOutput.debug.localChangesToCommit[0].status ===
-      common.FileStatusEnum.Modified;
+  });
 
   if (isPass === false) {
     console.log(context.stdout.toString());
