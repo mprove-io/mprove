b2cf5449cd721284091001f13532a64c7a305d90
akalitenya <akalitenya@mprove.io>
2023-01-04 08:50:33 +0000

    store p4

--- apps/front/src/main.ts
+++ apps/front/src/main.ts
@@ -1,10 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableElfProdMode } from '@ngneat/elf';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+  enableElfProdMode();
 }
 
 platformBrowserDynamic()
