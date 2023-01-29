2666965bbbda304735c600932aaa72a0f4c71e60
akalitenya <akalitenya@mprove.io>
2023-01-29 15:57:10 +0000

    m63

--- apps/backend/src/app-migrations.ts
+++ apps/backend/src/app-migrations.ts
@@ -3,11 +3,13 @@ import { All1669375708480 } from './migrations/1669375708480-All';
 import { All1669843639161 } from './migrations/1669843639161-All';
 import { All1673695457417 } from './migrations/1673695457417-All';
 import { All1674211821946 } from './migrations/1674211821946-All';
+import { All1675007717001 } from './migrations/1675007717001-All';
 
 export const appMigrations = [
   All1664996618229,
   All1669375708480,
   All1669843639161,
   All1673695457417,
-  All1674211821946
+  All1674211821946,
+  All1675007717001
 ];
