f45e755262460d98bc3e0b783b97d10e4a91c196
akalitenya <akalitenya@mprove.io>
2023-02-10 13:38:17 +0000

    m109

--- apps/front/src/app/modules/metrics/rep/rep.component.ts
+++ apps/front/src/app/modules/metrics/rep/rep.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  HostListener,
+  ViewChild
+} from '@angular/core';
 import { AgGridAngular } from 'ag-grid-angular';
 import { CellClickedEvent, ColDef, GridReadyEvent } from 'ag-grid-community';
 import { map, tap } from 'rxjs';
@@ -15,6 +20,11 @@ import { StatusRendererComponent } from '../status-renderer/status-renderer.comp
   templateUrl: './rep.component.html'
 })
 export class RepComponent {
+  @HostListener('window:keyup.esc')
+  onEscKeyUp() {
+    this.agGrid.api.deselectAll();
+  }
+
   rep: common.RepX;
   rep$ = this.repQuery.select().pipe(
     tap(x => {
@@ -27,7 +37,6 @@ export class RepComponent {
     {
       field: 'idx',
       rowDrag: true,
-      editable: false,
       resizable: false,
       pinned: 'left',
       width: 90,
@@ -37,13 +46,11 @@ export class RepComponent {
 
     {
       field: 'metric',
-      // suppressMovable: false,
       pinned: 'left',
       width: 600
     },
     {
       field: 'parameters',
-      // suppressMovable: false,
       pinned: 'left',
       width: 200
     },
@@ -65,16 +72,16 @@ export class RepComponent {
     // { field: 'jan2023', headerName: 'Jan 2023' },
   ];
 
-  // DefaultColDef sets props common to all Columns
   defaultColDef: ColDef = {
     // sortable: true,
     // filter: true,
     suppressMovable: true,
     resizable: true,
-    editable: true
+    editable: false,
+    suppressHeaderKeyboardEvent: params => true,
+    suppressKeyboardEvent: params => true
   };
 
-  // Data that gets displayed in the grid
   rowData$ = this.repQuery.select().pipe(
     tap(x => {
       let timeState = this.timeQuery.getValue();
@@ -124,41 +131,16 @@ export class RepComponent {
     private metricsQuery: MetricsQuery
   ) {}
 
-  // Example load data from server
   onGridReady(params: GridReadyEvent) {
     // this.agGrid.api.sizeColumnsToFit();
-    // let metrics = this.metricsQuery.getValue();
-    // let rep = this.repQuery.getValue();
-    // let data = rep.rows.map(row => {
-    //   console.log(row);
-    //   console.log(metrics.metrics);
-    //   let metric = metrics.metrics.find(m => m.metricId === row.metricId);
-    //   let dataRow = {
-    //     idx: row.rowId,
-    //     parameters: '',
-    //     metric: metric?.label || row.metricId,
-    //     jan2023: 72000,
-    //     feb2023: 72000,
-    //     mar2023: 72000,
-    //     apr2023: 72000
-    //   };
-    //   return dataRow;
-    // });
-    // this.rowData$ = of(data);
   }
 
-  // Example of consuming Grid Event
   onCellClicked(e: CellClickedEvent): void {
-    console.log('cellClicked', e);
+    // console.log('cellClicked', e);
   }
 
   rowDragEndHandle(e: any): void {
-    console.log('rowDragEndHandle', e);
-    console.log(this.agGrid.api.getDisplayedRowAtIndex(0));
-  }
-
-  // Example using Grid's API
-  clearSelection(): void {
-    this.agGrid.api.deselectAll();
+    // console.log('rowDragEndHandle', e);
+    // console.log(this.agGrid.api.getDisplayedRowAtIndex(0));
   }
 }
