518fc129ceda134ee73787259f8032fff996a89e
akalitenya <akalitenya@mprove.io>
2023-04-04 11:39:26 +0000

    m303

--- apps/blockml/src/models/17-rep/04-check-rep-row.ts
+++ apps/blockml/src/models/17-rep/04-check-rep-row.ts
@@ -44,6 +44,62 @@ export function checkRepRow(
         );
         return;
       }
+
+      if (common.isUndefined(row.type)) {
+        item.errors.push(
+          new BmError({
+            title: common.ErTitleEnum.MISSING_ROW_TYPE,
+            message: `parameter "${common.ParameterEnum.Type}" is required for a row`,
+            lines: [
+              {
+                line: row.row_id_line_num,
+                name: x.fileName,
+                path: x.filePath
+              }
+            ]
+          })
+        );
+        return;
+      }
+      //  else if (common.MEASURE_TYPE_VALUES.indexOf(field.type) < 0) {
+      //   item.errors.push(
+      //     new BmError({
+      //       title: common.ErTitleEnum.WRONG_MEASURE_TYPE,
+      //       message: `"${field.type}" value is not valid ${common.ParameterEnum.Type} for measure`,
+      //       lines: [
+      //         {
+      //           line: field.type_line_num,
+      //           name: x.fileName,
+      //           path: x.filePath
+      //         }
+      //       ]
+      //     })
+      //   );
+      //   return;
+      // } else if (
+      //   !field.sql_key &&
+      //   [
+      //     common.FieldTypeEnum.SumByKey,
+      //     common.FieldTypeEnum.AverageByKey,
+      //     common.FieldTypeEnum.MedianByKey,
+      //     common.FieldTypeEnum.PercentileByKey
+      //   ].indexOf(field.type) > -1
+      // ) {
+      //   item.errors.push(
+      //     new BmError({
+      //       title: common.ErTitleEnum.MISSING_SQL_KEY,
+      //       message: `parameter "${common.ParameterEnum.SqlKey}" is required for measure of type "${field.type}"`,
+      //       lines: [
+      //         {
+      //           line: field.name_line_num,
+      //           name: x.fileName,
+      //           path: x.filePath
+      //         }
+      //       ]
+      //     })
+      //   );
+      //   return;
+      // }
     });
 
     if (errorsOnStart === item.errors.length) {
