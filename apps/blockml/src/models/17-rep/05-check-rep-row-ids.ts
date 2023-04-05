15b0b641483bef5ec1619484e13f3c6543f7c450
akalitenya <akalitenya@mprove.io>
2023-04-05 07:20:38 +0000

    m312

--- apps/blockml/src/models/17-rep/05-check-rep-row-ids.ts
+++ apps/blockml/src/models/17-rep/05-check-rep-row-ids.ts
@@ -23,19 +23,34 @@ export function checkRepRowIds(
   item.reps.forEach(x => {
     let errorsOnStart = item.errors.length;
 
+    let rowIdMaps: Array<{ rowId: string; lineNumbers: number[] }> = [];
+
     x.rows.forEach(row => {
-      if (!!row.row_id.match(common.MyRegex.CONTAINS_A_to_Z()) === false) {
+      let rowIdMap = rowIdMaps.find(element => element.rowId === row.row_id);
+
+      if (rowIdMap) {
+        rowIdMap.lineNumbers.push(row.row_id_line_num);
+      } else {
+        rowIdMaps.push({
+          rowId: row.row_id,
+          lineNumbers: [row.row_id_line_num]
+        });
+      }
+    });
+
+    rowIdMaps.forEach(n => {
+      if (n.lineNumbers.length > 1) {
+        let lines: common.FileErrorLine[] = n.lineNumbers.map(y => ({
+          line: y,
+          name: x.fileName,
+          path: x.filePath
+        }));
+
         item.errors.push(
           new BmError({
-            title: common.ErTitleEnum.WRONG_CHARS_IN_ROW_ID,
-            message: `parameter "${common.ParameterEnum.RowId}" must consist of characters A-Z`,
-            lines: [
-              {
-                line: row.row_id_line_num,
-                name: x.fileName,
-                path: x.filePath
-              }
-            ]
+            title: common.ErTitleEnum.DUPLICATE_ROW_IDS,
+            message: 'Each row must have a unique row_id',
+            lines: lines
           })
         );
         return;
@@ -43,34 +58,19 @@ export function checkRepRowIds(
     });
 
     if (item.errors.length === errorsOnStart) {
-      let rowIdMaps: Array<{ rowId: string; lineNumbers: number[] }> = [];
-
       x.rows.forEach(row => {
-        let rowIdMap = rowIdMaps.find(element => element.rowId === row.row_id);
-
-        if (rowIdMap) {
-          rowIdMap.lineNumbers.push(row.row_id_line_num);
-        } else {
-          rowIdMaps.push({
-            rowId: row.row_id,
-            lineNumbers: [row.row_id_line_num]
-          });
-        }
-      });
-
-      rowIdMaps.forEach(n => {
-        if (n.lineNumbers.length > 1) {
-          let lines: common.FileErrorLine[] = n.lineNumbers.map(y => ({
-            line: y,
-            name: x.fileName,
-            path: x.filePath
-          }));
-
+        if (!!row.row_id.match(common.MyRegex.CONTAINS_A_to_Z()) === false) {
           item.errors.push(
             new BmError({
-              title: common.ErTitleEnum.DUPLICATE_ROW_IDS,
-              message: 'Each row must have a unique row_id',
-              lines: lines
+              title: common.ErTitleEnum.WRONG_CHARS_IN_ROW_ID,
+              message: `parameter "${common.ParameterEnum.RowId}" must consist of characters A-Z`,
+              lines: [
+                {
+                  line: row.row_id_line_num,
+                  name: x.fileName,
+                  path: x.filePath
+                }
+              ]
             })
           );
           return;
