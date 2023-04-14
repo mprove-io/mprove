b5f1b8b1a6516d2f3299e5254ddb3c8f564f764b
akalitenya <akalitenya@mprove.io>
2023-04-14 08:00:28 +0000

    m362

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,6 @@
 sh scripts/werf-kube-run-test-disk-dev.sh \
   && sh scripts/werf-kube-run-test-blockml-dev.sh \
-  && sh scripts/werf-kube-run-ava-mcli-dev.sh \
   && sh scripts/werf-kube-run-e2e-backend-dev.sh \
   && sh scripts/werf-kube-run-e2e-integra-dev.sh
+  
+  # && sh scripts/werf-kube-run-ava-mcli-dev.sh \
