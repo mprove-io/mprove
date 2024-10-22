485f536419aa51ed1eba1a71fc27027e06e6ad55
akalitenya <akalitenya@mprove.io>
2024-10-22 15:11:55 +0000

    m453

--- apps/front/src/app/modules/metrics/rep/data-renderer/data-renderer.component.ts
+++ apps/front/src/app/modules/metrics/rep/data-renderer/data-renderer.component.ts
@@ -1,6 +1,9 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { ICellRendererAngularComp } from 'ag-grid-angular';
 import { ICellRendererParams } from 'ag-grid-community';
+import { MetricsQuery } from '~front/app/queries/metrics.query';
+import { RepQuery } from '~front/app/queries/rep.query';
+import { MconfigService } from '~front/app/services/mconfig.service';
 import { QueryService } from '~front/app/services/query.service';
 import { common } from '~front/barrels/common';
 import { DataRow } from '../rep.component';
@@ -12,6 +15,7 @@ import { DataRow } from '../rep.component';
 export class DataRendererComponent implements ICellRendererAngularComp {
   params: ICellRendererParams<DataRow>;
 
+  rowTypeMetric = common.RowTypeEnum.Metric;
   formattedValue: string;
   isError = false;
 
@@ -46,8 +50,58 @@ export class DataRendererComponent implements ICellRendererAngularComp {
         : undefined;
   }
 
+  clickData() {
+    if (
+      common.isDefined(this.params.data.mconfig) &&
+      this.params.data.rowType === common.RowTypeEnum.Metric
+    ) {
+      let metric = this.metricsQuery
+        .getValue()
+        .metrics.find(y => y.metricId === this.params.data.metricId);
+
+      let timeSpec = this.repQuery.getValue().timeSpec;
+
+      let timeSpecWord = common.getTimeSpecWord({ timeSpec: timeSpec });
+
+      let timeFieldIdSpec = `${metric.timeFieldId}${common.TRIPLE_UNDERSCORE}${timeSpecWord}`;
+
+      let newMconfigId = common.makeId();
+      let newQueryId = common.makeId();
+
+      let mconfigCopy = common.makeCopy(this.params.data.mconfig);
+
+      let mconfig = Object.assign(mconfigCopy, <common.MconfigX>{
+        mconfigId: newMconfigId,
+        queryId: newQueryId,
+        temp: true,
+        serverTs: 1
+      });
+
+      let timeFilter = mconfig.filters.find(
+        filter => filter.fieldId === timeFieldIdSpec
+      );
+
+      // let newFraction = {
+      //   // brick: `on ${this.getYearStr({ dateValue: this.dateStr })}`,
+      //   brick: `on 2023`,
+      //   operator: common.FractionOperatorEnum.Or,
+      //   type: common.FractionTypeEnum.TsIsOnYear,
+      //   // tsDateYear: Number(this.dateStr.split('-')[0])
+      //   tsDateYear: Number(2023)
+      // };
+
+      // timeFilter.fractions = [newFraction];
+
+      let newMconfig = mconfig;
+
+      this.mconfigService.navCreateTempMconfigAndQuery(newMconfig);
+    }
+  }
+
   constructor(
-    private cd: ChangeDetectorRef,
-    private queryService: QueryService
+    private queryService: QueryService,
+    private mconfigService: MconfigService,
+    private metricsQuery: MetricsQuery,
+    private repQuery: RepQuery
   ) {}
 }
