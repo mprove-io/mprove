3d4cf2360d64ace4dac349da20bba584136a1b08
akalitenya <akalitenya@mprove.io>
2024-11-02 05:10:11 +0000

    m488

--- libs/common/src/constants/chart-data-type-specific-parameters/y-field-chart-types.ts
+++ libs/common/src/constants/chart-data-type-specific-parameters/y-field-chart-types.ts
@@ -1,7 +1,13 @@
 import { enums } from '~common/barrels/enums';
 
 export const yFieldChartTypes = [
-  enums.ChartTypeEnum.AgBar,
+  // enums.ChartTypeEnum.AgLine,
+  // enums.ChartTypeEnum.AgArea,
+  // enums.ChartTypeEnum.AgBar,
+  enums.ChartTypeEnum.AgScatter,
+  enums.ChartTypeEnum.AgBubble,
+  enums.ChartTypeEnum.AgPie,
+  enums.ChartTypeEnum.AgDonut,
   // ===
   enums.ChartTypeEnum.BarVertical,
   enums.ChartTypeEnum.BarHorizontal,
