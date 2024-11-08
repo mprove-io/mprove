21e14166ed009acd24c49e3db81129d7f1be26e7
akalitenya <akalitenya@mprove.io>
2024-11-08 06:38:26 +0000

    m534

--- apps/backend/src/controllers/envs/delete-env/delete-env.controller.ts
+++ apps/backend/src/controllers/envs/delete-env/delete-env.controller.ts
@@ -1,25 +1,42 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Inject,
+  Logger,
+  Post,
+  Req,
+  UseGuards
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { and, eq } from 'drizzle-orm';
 import { apiToBackend } from '~backend/barrels/api-to-backend';
 import { common } from '~backend/barrels/common';
-import { repositories } from '~backend/barrels/repositories';
+import { interfaces } from '~backend/barrels/interfaces';
+import { schemaPostgres } from '~backend/barrels/schema-postgres';
 import { AttachUser } from '~backend/decorators/_index';
+import { DRIZZLE, Db } from '~backend/drizzle/drizzle.module';
+import { bridgesTable } from '~backend/drizzle/postgres/schema/bridges';
+import { envsTable } from '~backend/drizzle/postgres/schema/envs';
+import { getRetryOption } from '~backend/functions/get-retry-option';
 import { ValidateRequestGuard } from '~backend/guards/validate-request.guard';
 import { MembersService } from '~backend/services/members.service';
 import { ProjectsService } from '~backend/services/projects.service';
+
+let retry = require('async-retry');
 
 @UseGuards(ValidateRequestGuard)
 @Controller()
 export class DeleteEnvController {
   constructor(
     private projectsService: ProjectsService,
-    private envsRepository: repositories.EnvsRepository,
     private membersService: MembersService,
-    private bridgesRepository: repositories.BridgesRepository
+    private cs: ConfigService<interfaces.Config>,
+    private logger: Logger,
+    @Inject(DRIZZLE) private db: Db
   ) {}
 
   @Post(apiToBackend.ToBackendRequestInfoNameEnum.ToBackendDeleteEnv)
   async deleteEnv(
-    @AttachUser() user: schemaPostgres.UserEntity,
+    @AttachUser() user: schemaPostgres.UserEnt,
     @Req() request: any
   ) {
     let reqValid: apiToBackend.ToBackendDeleteEnvRequest = request.body;
@@ -31,7 +48,7 @@ export class DeleteEnvController {
     });
 
     await this.membersService.checkMemberIsAdmin({
-      memberId: user.user_id,
+      memberId: user.userId,
       projectId: projectId
     });
 
@@ -41,15 +58,39 @@ export class DeleteEnvController {
       });
     }
 
-    await this.envsRepository.delete({
-      project_id: projectId,
-      env_id: envId
-    });
+    await retry(
+      async () =>
+        await this.db.drizzle.transaction(async tx => {
+          await tx
+            .delete(envsTable)
+            .where(
+              and(
+                eq(envsTable.projectId, projectId),
+                eq(envsTable.envId, envId)
+              )
+            );
 
-    await this.bridgesRepository.delete({
-      project_id: projectId,
-      env_id: envId
-    });
+          await tx
+            .delete(bridgesTable)
+            .where(
+              and(
+                eq(bridgesTable.projectId, projectId),
+                eq(bridgesTable.envId, envId)
+              )
+            );
+        }),
+      getRetryOption(this.cs, this.logger)
+    );
+
+    // await this.envsRepository.delete({
+    //   project_id: projectId,
+    //   env_id: envId
+    // });
+
+    // await this.bridgesRepository.delete({
+    //   project_id: projectId,
+    //   env_id: envId
+    // });
 
     let payload = {};
 
--- apps/backend/src/controllers/envs/get-envs-list/get-envs-list.controller.ts
+++ apps/backend/src/controllers/envs/get-envs-list/get-envs-list.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
-import { In } from 'typeorm';
+import { Controller, Inject, Post, Req, UseGuards } from '@nestjs/common';
+import { and, eq, inArray } from 'drizzle-orm';
 import { apiToBackend } from '~backend/barrels/api-to-backend';
 import { common } from '~backend/barrels/common';
-import { repositories } from '~backend/barrels/repositories';
-import { wrapper } from '~backend/barrels/wrapper';
+import { schemaPostgres } from '~backend/barrels/schema-postgres';
 import { AttachUser } from '~backend/decorators/_index';
+import { DRIZZLE, Db } from '~backend/drizzle/drizzle.module';
+import { envsTable } from '~backend/drizzle/postgres/schema/envs';
 import { ValidateRequestGuard } from '~backend/guards/validate-request.guard';
 import { MembersService } from '~backend/services/members.service';
 import { ProjectsService } from '~backend/services/projects.service';
+import { WrapToApiService } from '~backend/services/wrap-to-api.service';
 
 @UseGuards(ValidateRequestGuard)
 @Controller()
@@ -15,12 +17,13 @@ export class GetEnvsListController {
   constructor(
     private projectsService: ProjectsService,
     private membersService: MembersService,
-    private envsRepository: repositories.EnvsRepository
+    private wrapToApiService: WrapToApiService,
+    @Inject(DRIZZLE) private db: Db
   ) {}
 
   @Post(apiToBackend.ToBackendRequestInfoNameEnum.ToBackendGetEnvsList)
   async getEnvsList(
-    @AttachUser() user: schemaPostgres.UserEntity,
+    @AttachUser() user: schemaPostgres.UserEnt,
     @Req() request: any
   ) {
     let reqValid: apiToBackend.ToBackendGetEnvsListRequest = request.body;
@@ -33,30 +36,41 @@ export class GetEnvsListController {
 
     let member = await this.membersService.getMemberCheckExists({
       projectId: projectId,
-      memberId: user.user_id
+      memberId: user.userId
     });
 
-    let envs: schemaPostgres.EnvEntity[] = [];
+    let envs: schemaPostgres.EnvEnt[] = [];
 
     if (isFilter === true) {
-      envs = await this.envsRepository.find({
-        where: {
-          project_id: projectId,
-          env_id: In([...member.envs, common.PROJECT_ENV_PROD])
-        }
+      envs = await this.db.drizzle.query.envsTable.findMany({
+        where: and(
+          eq(envsTable.projectId, projectId),
+          inArray(envsTable.envId, [...member.envs, common.PROJECT_ENV_PROD])
+        )
       });
+
+      // envs = await this.envsRepository.find({
+      //   where: {
+      //     project_id: projectId,
+      //     env_id: In([...member.envs, common.PROJECT_ENV_PROD])
+      //   }
+      // });
     } else {
-      envs = await this.envsRepository.find({
-        where: { project_id: projectId }
+      envs = await this.db.drizzle.query.envsTable.findMany({
+        where: eq(envsTable.projectId, projectId)
       });
+
+      // envs = await this.envsRepository.find({
+      //   where: { project_id: projectId }
+      // });
     }
 
     let sortedEnvs = envs.sort((a, b) =>
-      a.env_id > b.env_id ? 1 : b.env_id > a.env_id ? -1 : 0
+      a.envId > b.envId ? 1 : b.envId > a.envId ? -1 : 0
     );
 
     let payload: apiToBackend.ToBackendGetEnvsListResponsePayload = {
-      envsList: sortedEnvs.map(x => wrapper.wrapToApiEnvsItem(x))
+      envsList: sortedEnvs.map(x => this.wrapToApiService.wrapToApiEnvsItem(x))
     };
 
     return payload;
