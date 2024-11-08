536023abe3eb523f48b0c44b26c5beb3099d24f7
akalitenya <akalitenya@mprove.io>
2024-11-08 11:26:05 +0000

    m535

--- apps/backend/src/controllers/evs/create-ev/create-ev.controller.ts
+++ apps/backend/src/controllers/evs/create-ev/create-ev.controller.ts
@@ -1,36 +1,50 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Inject,
+  Logger,
+  Post,
+  Req,
+  UseGuards
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { and, eq } from 'drizzle-orm';
 import { forEachSeries } from 'p-iteration';
 import { apiToBackend } from '~backend/barrels/api-to-backend';
 import { common } from '~backend/barrels/common';
 import { interfaces } from '~backend/barrels/interfaces';
-import { maker } from '~backend/barrels/maker';
-import { repositories } from '~backend/barrels/repositories';
-import { wrapper } from '~backend/barrels/wrapper';
+import { schemaPostgres } from '~backend/barrels/schema-postgres';
 import { AttachUser } from '~backend/decorators/_index';
+import { DRIZZLE, Db } from '~backend/drizzle/drizzle.module';
+import { bridgesTable } from '~backend/drizzle/postgres/schema/bridges';
+import { getRetryOption } from '~backend/functions/get-retry-option';
 import { ValidateRequestGuard } from '~backend/guards/validate-request.guard';
-import { DbService } from '~backend/services/db.service';
 import { EnvsService } from '~backend/services/envs.service';
 import { EvsService } from '~backend/services/evs.service';
+import { MakerService } from '~backend/services/maker.service';
 import { MembersService } from '~backend/services/members.service';
 import { ProjectsService } from '~backend/services/projects.service';
+import { WrapToApiService } from '~backend/services/wrap-to-api.service';
+
+let retry = require('async-retry');
 
 @UseGuards(ValidateRequestGuard)
 @Controller()
 export class CreateEvController {
   constructor(
     private projectsService: ProjectsService,
-    private bridgesRepository: repositories.BridgesRepository,
     private envsService: EnvsService,
     private evsService: EvsService,
-    private cs: ConfigService<interfaces.Config>,
     private membersService: MembersService,
-    private dbService: DbService
+    private makerService: MakerService,
+    private cs: ConfigService<interfaces.Config>,
+    private logger: Logger,
+    private wrapToApiService: WrapToApiService,
+    @Inject(DRIZZLE) private db: Db
   ) {}
 
   @Post(apiToBackend.ToBackendRequestInfoNameEnum.ToBackendCreateEv)
   async createEv(
-    @AttachUser() user: schemaPostgres.UserEntity,
+    @AttachUser() user: schemaPostgres.UserEnt,
     @Req() request: any
   ) {
     let reqValid: apiToBackend.ToBackendCreateEvRequest = request.body;
@@ -42,17 +56,14 @@ export class CreateEvController {
     });
 
     let member = await this.membersService.getMemberCheckIsEditorOrAdmin({
-      memberId: user.user_id,
+      memberId: user.userId,
       projectId: projectId
     });
 
     let firstProjectId =
       this.cs.get<interfaces.Config['firstProjectId']>('firstProjectId');
 
-    if (
-      member.is_admin === common.BoolEnum.FALSE &&
-      projectId === firstProjectId
-    ) {
+    if (member.isAdmin === false && projectId === firstProjectId) {
       throw new common.ServerError({
         message: common.ErEnum.BACKEND_RESTRICTED_PROJECT
       });
@@ -70,40 +81,64 @@ export class CreateEvController {
       evId: evId
     });
 
-    let newEv = maker.makeEv({
+    let newEv = this.makerService.makeEv({
       projectId: projectId,
       envId: envId,
       evId: evId,
       val: val
     });
 
-    let branchBridges = await this.bridgesRepository.find({
-      where: {
-        project_id: projectId,
-        env_id: envId
-      }
+    let branchBridges = await this.db.drizzle.query.bridgesTable.findMany({
+      where: and(
+        eq(bridgesTable.projectId, projectId),
+        eq(bridgesTable.envId, envId)
+      )
     });
+
+    // let branchBridges = await this.bridgesRepository.find({
+    //   where: {
+    //     project_id: projectId,
+    //     env_id: envId
+    //   }
+    // });
 
     await forEachSeries(branchBridges, async x => {
-      x.need_validate = common.BoolEnum.TRUE;
+      x.needValidate = true;
     });
 
-    await this.dbService.writeRecords({
-      modify: true,
-      records: {
-        bridges: [...branchBridges]
-      }
-    });
+    await retry(
+      async () =>
+        await this.db.drizzle.transaction(
+          async tx =>
+            await this.db.packer.write({
+              tx: tx,
+              insert: {
+                evs: [newEv]
+              },
+              insertOrUpdate: {
+                bridges: [...branchBridges]
+              }
+            })
+        ),
+      getRetryOption(this.cs, this.logger)
+    );
 
-    await this.dbService.writeRecords({
-      modify: false,
-      records: {
-        evs: [newEv]
-      }
-    });
+    // await this.dbService.writeRecords({
+    //   modify: true,
+    //   records: {
+    //     bridges: [...branchBridges]
+    //   }
+    // });
+
+    // await this.dbService.writeRecords({
+    //   modify: false,
+    //   records: {
+    //     evs: [newEv]
+    //   }
+    // });
 
     let payload: apiToBackend.ToBackendCreateEvResponsePayload = {
-      ev: wrapper.wrapToApiEv(newEv)
+      ev: this.wrapToApiService.wrapToApiEv(newEv)
     };
 
     return payload;
--- apps/backend/src/controllers/evs/get-evs/get-evs.controller.ts
+++ apps/backend/src/controllers/evs/get-evs/get-evs.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, Inject, Post, Req, UseGuards } from '@nestjs/common';
+import { and, eq } from 'drizzle-orm';
 import { apiToBackend } from '~backend/barrels/api-to-backend';
-import { repositories } from '~backend/barrels/repositories';
-import { wrapper } from '~backend/barrels/wrapper';
+import { schemaPostgres } from '~backend/barrels/schema-postgres';
 import { AttachUser } from '~backend/decorators/_index';
+import { DRIZZLE, Db } from '~backend/drizzle/drizzle.module';
+import { evsTable } from '~backend/drizzle/postgres/schema/evs';
 import { ValidateRequestGuard } from '~backend/guards/validate-request.guard';
 import { EnvsService } from '~backend/services/envs.service';
 import { MembersService } from '~backend/services/members.service';
 import { ProjectsService } from '~backend/services/projects.service';
+import { WrapToApiService } from '~backend/services/wrap-to-api.service';
 
 @UseGuards(ValidateRequestGuard)
 @Controller()
@@ -15,12 +18,13 @@ export class GetEvsController {
     private projectsService: ProjectsService,
     private membersService: MembersService,
     private envsService: EnvsService,
-    private evsRepository: repositories.EvsRepository
+    private wrapToApiService: WrapToApiService,
+    @Inject(DRIZZLE) private db: Db
   ) {}
 
   @Post(apiToBackend.ToBackendRequestInfoNameEnum.ToBackendGetEvs)
   async getEvs(
-    @AttachUser() user: schemaPostgres.UserEntity,
+    @AttachUser() user: schemaPostgres.UserEnt,
     @Req() request: any
   ) {
     let reqValid: apiToBackend.ToBackendGetEvsRequest = request.body;
@@ -33,7 +37,7 @@ export class GetEvsController {
 
     let userMember = await this.membersService.getMemberCheckExists({
       projectId: projectId,
-      memberId: user.user_id
+      memberId: user.userId
     });
 
     let env = await this.envsService.getEnvCheckExistsAndAccess({
@@ -42,18 +46,22 @@ export class GetEvsController {
       member: userMember
     });
 
-    let evs = await this.evsRepository.find({
-      where: {
-        project_id: projectId,
-        env_id: envId
-      }
+    let evs = await this.db.drizzle.query.evsTable.findMany({
+      where: and(eq(evsTable.projectId, projectId), eq(evsTable.envId, envId))
     });
 
-    let apiMember = wrapper.wrapToApiMember(userMember);
+    // let evs = await this.evsRepository.find({
+    //   where: {
+    //     project_id: projectId,
+    //     env_id: envId
+    //   }
+    // });
+
+    let apiMember = this.wrapToApiService.wrapToApiMember(userMember);
 
     let payload: apiToBackend.ToBackendGetEvsResponsePayload = {
       userMember: apiMember,
-      evs: evs.map(x => wrapper.wrapToApiEv(x))
+      evs: evs.map(x => this.wrapToApiService.wrapToApiEv(x))
     };
 
     return payload;
