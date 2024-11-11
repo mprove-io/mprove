c1ca589969ab435297a559c43d0b4af18e46b62c
akalitenya <akalitenya@mprove.io>
2024-11-11 13:19:47 +0000

    m582

---
+++ apps/backend/src/wait-for-db.ts
@@ -0,0 +1,30 @@
+import 'reflect-metadata';
+//
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  connectionString: process.env.CLI_DRIZZLE_POSTGRES_DATABASE_URL
+});
+
+const db = drizzle(pool);
+
+async function checkDatabase() {
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    try {
+      await db.execute('SELECT 1');
+      console.log('Database is ready!');
+      break; // Exit the loop if the query is successful
+    } catch (error) {
+      console.log('Waiting for database...');
+      await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 second before retrying
+    }
+  }
+
+  await pool.end();
+}
+
+checkDatabase().catch(err => {
+  console.error('Error checking database:', err);
+});
