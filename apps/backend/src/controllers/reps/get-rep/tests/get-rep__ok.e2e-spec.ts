b377305cb6d6ef4eac5dc99d01dc06c532ee6df7
akalitenya <akalitenya@mprove.io>
2024-11-12 10:10:43 +0000

    m586

---
+++ apps/backend/src/controllers/reps/get-rep/tests/get-rep__ok.e2e-spec.ts
@@ -0,0 +1,125 @@
+import test from 'ava';
+import { apiToBackend } from '~backend/barrels/api-to-backend';
+import { common } from '~backend/barrels/common';
+import { helper } from '~backend/barrels/helper';
+import { interfaces } from '~backend/barrels/interfaces';
+import { logToConsoleBackend } from '~backend/functions/log-to-console-backend';
+import { prepareTest } from '~backend/functions/prepare-test';
+
+let testId = 'backend-get-rep__ok';
+
+let traceId = testId;
+
+let userId = common.makeId();
+let email = `${testId}@example.com`;
+let password = '123456';
+
+let orgId = testId;
+let orgName = testId;
+
+let testProjectId = 't1';
+let projectId = common.makeId();
+let projectName = testId;
+
+let prep: interfaces.Prep;
+
+test('1', async t => {
+  let resp: apiToBackend.ToBackendGetRepResponse;
+
+  try {
+    prep = await prepareTest({
+      traceId: traceId,
+      deleteRecordsPayload: {
+        idempotencyKeys: [testId],
+        emails: [email],
+        orgIds: [orgId],
+        projectIds: [projectId],
+        projectNames: [projectName]
+      },
+      seedRecordsPayload: {
+        users: [
+          {
+            userId,
+            email,
+            password,
+            isEmailVerified: common.BoolEnum.TRUE
+          }
+        ],
+        orgs: [
+          {
+            orgId: orgId,
+            ownerEmail: email,
+            name: orgName
+          }
+        ],
+        projects: [
+          {
+            orgId,
+            projectId,
+            testProjectId,
+            name: projectName,
+            defaultBranch: common.BRANCH_MASTER,
+            remoteType: common.ProjectRemoteTypeEnum.Managed
+          }
+        ],
+        members: [
+          {
+            memberId: userId,
+            email,
+            projectId,
+            isAdmin: common.BoolEnum.TRUE,
+            isEditor: common.BoolEnum.TRUE,
+            isExplorer: common.BoolEnum.TRUE
+          }
+        ],
+        connections: [
+          {
+            projectId: projectId,
+            connectionId: 'c1',
+            envId: common.PROJECT_ENV_PROD,
+            type: common.ConnectionTypeEnum.PostgreSQL
+          }
+        ]
+      },
+      loginUserPayload: { email, password }
+    });
+
+    let req: apiToBackend.ToBackendGetRepRequest = {
+      info: {
+        name: apiToBackend.ToBackendRequestInfoNameEnum.ToBackendGetRep,
+        traceId: traceId,
+        idempotencyKey: testId
+      },
+      payload: {
+        projectId: projectId,
+        branchId: common.BRANCH_MASTER,
+        envId: common.PROJECT_ENV_PROD,
+        isRepoProd: false,
+        repId: 'r1',
+        timeRangeFractionBrick: 'last 5 months',
+        timeSpec: common.TimeSpecEnum.Months,
+        timezone: 'UTC'
+      }
+    };
+
+    resp = await helper.sendToBackend<apiToBackend.ToBackendGetRepResponse>({
+      httpServer: prep.httpServer,
+      loginToken: prep.loginToken,
+      req: req
+    });
+
+    // console.log(resp);
+
+    await prep.app.close();
+  } catch (e) {
+    logToConsoleBackend({
+      log: e,
+      logLevel: common.LogLevelEnum.Error,
+      logger: prep.logger,
+      cs: prep.cs
+    });
+  }
+
+  t.is(resp.info.error, undefined);
+  t.is(resp.info.status, common.ResponseInfoStatusEnum.Ok);
+});
