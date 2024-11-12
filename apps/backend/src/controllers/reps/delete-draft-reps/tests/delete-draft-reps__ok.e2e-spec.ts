301fc71c10d42272a1d87fa3566b12b757aec442
akalitenya <akalitenya@mprove.io>
2024-11-12 09:34:42 +0000

    m585

---
+++ apps/backend/src/controllers/reps/delete-draft-reps/tests/delete-draft-reps__ok.e2e-spec.ts
@@ -0,0 +1,153 @@
+import test from 'ava';
+import { apiToBackend } from '~backend/barrels/api-to-backend';
+import { common } from '~backend/barrels/common';
+import { helper } from '~backend/barrels/helper';
+import { interfaces } from '~backend/barrels/interfaces';
+import { logToConsoleBackend } from '~backend/functions/log-to-console-backend';
+import { prepareTest } from '~backend/functions/prepare-test';
+
+let testId = 'backend-delete-draft-reps__ok';
+
+let traceId = testId;
+
+let userId = common.makeId();
+let email = `${testId}@example.com`;
+let password = '123456';
+
+let orgId = testId;
+let orgName = testId;
+
+let testProjectId = 't1';
+let projectId = common.makeId();
+let projectName = testId;
+
+let prep: interfaces.Prep;
+
+test('1', async t => {
+  let resp2: apiToBackend.ToBackendDeleteDraftRepsResponse;
+
+  try {
+    prep = await prepareTest({
+      traceId: traceId,
+      deleteRecordsPayload: {
+        idempotencyKeys: [testId],
+        emails: [email],
+        orgIds: [orgId],
+        projectIds: [projectId],
+        projectNames: [projectName]
+      },
+      seedRecordsPayload: {
+        users: [
+          {
+            userId,
+            email,
+            password,
+            isEmailVerified: common.BoolEnum.TRUE
+          }
+        ],
+        orgs: [
+          {
+            orgId: orgId,
+            ownerEmail: email,
+            name: orgName
+          }
+        ],
+        projects: [
+          {
+            orgId,
+            projectId,
+            testProjectId,
+            name: projectName,
+            defaultBranch: common.BRANCH_MASTER,
+            remoteType: common.ProjectRemoteTypeEnum.Managed
+          }
+        ],
+        members: [
+          {
+            memberId: userId,
+            email,
+            projectId,
+            isAdmin: common.BoolEnum.TRUE,
+            isEditor: common.BoolEnum.TRUE,
+            isExplorer: common.BoolEnum.TRUE
+          }
+        ],
+        connections: [
+          {
+            projectId: projectId,
+            connectionId: 'c1',
+            envId: common.PROJECT_ENV_PROD,
+            type: common.ConnectionTypeEnum.PostgreSQL
+          }
+        ]
+      },
+      loginUserPayload: { email, password }
+    });
+
+    let req1: apiToBackend.ToBackendCreateDraftRepRequest = {
+      info: {
+        name: apiToBackend.ToBackendRequestInfoNameEnum.ToBackendCreateDraftRep,
+        traceId: traceId,
+        idempotencyKey: testId
+      },
+      payload: {
+        projectId: projectId,
+        isRepoProd: false,
+        branchId: common.BRANCH_MASTER,
+        envId: common.PROJECT_ENV_PROD,
+        rowIds: undefined,
+        changeType: common.ChangeTypeEnum.AddEmpty,
+        fromRepId: 'new',
+        rowChange: { rowType: common.RowTypeEnum.Empty, showChart: false },
+        timeRangeFractionBrick: 'last 5 months',
+        timeSpec: common.TimeSpecEnum.Months,
+        timezone: 'UTC'
+      }
+    };
+
+    let resp1 =
+      await helper.sendToBackend<apiToBackend.ToBackendCreateDraftRepResponse>({
+        httpServer: prep.httpServer,
+        loginToken: prep.loginToken,
+        req: req1
+      });
+
+    let req2: apiToBackend.ToBackendDeleteDraftRepsRequest = {
+      info: {
+        name: apiToBackend.ToBackendRequestInfoNameEnum.ToBackendCreateDraftRep,
+        traceId: traceId,
+        idempotencyKey: testId
+      },
+      payload: {
+        projectId: projectId,
+        isRepoProd: false,
+        branchId: common.BRANCH_MASTER,
+        envId: common.PROJECT_ENV_PROD,
+        repIds: [resp1.payload.rep.repId]
+      }
+    };
+
+    resp2 =
+      await helper.sendToBackend<apiToBackend.ToBackendDeleteDraftRepsResponse>(
+        {
+          httpServer: prep.httpServer,
+          loginToken: prep.loginToken,
+          req: req1
+        }
+      );
+
+    // console.log(resp2);
+
+    await prep.app.close();
+  } catch (e) {
+    logToConsoleBackend({
+      log: e,
+      logLevel: common.LogLevelEnum.Error,
+      logger: prep.logger,
+      cs: prep.cs
+    });
+  }
+
+  t.is(resp2.info.error, undefined);
+  t.is(resp2.info.status, common.ResponseInfoStatusEnum.Ok);
+});
