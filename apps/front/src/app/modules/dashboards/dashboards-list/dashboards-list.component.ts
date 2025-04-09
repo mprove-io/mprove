c2a235a7293a2f807e373f3f32e74175cbf5eaef
akalitenya <akalitenya@mprove.io>
2025-04-09 17:59:55 +0000

    m1346

--- apps/front/src/app/modules/dashboards/dashboards-list/dashboards-list.component.ts
+++ apps/front/src/app/modules/dashboards/dashboards-list/dashboards-list.component.ts
@@ -1,8 +1,6 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { MemberQuery } from '~front/app/queries/member.query';
 import { NavQuery, NavState } from '~front/app/queries/nav.query';
-import { ApiService } from '~front/app/services/api.service';
-import { MyDialogService } from '~front/app/services/my-dialog.service';
 import { NavigateService } from '~front/app/services/navigate.service';
 import { common } from '~front/barrels/common';
 
@@ -41,46 +39,14 @@ export class DashboardsListComponent {
 
   constructor(
     private cd: ChangeDetectorRef,
-    private apiService: ApiService,
     private navQuery: NavQuery,
     private filteredDashboardsQuery: FilteredDashboardsQuery,
     private memberQuery: MemberQuery,
-    private myDialogService: MyDialogService,
     private navigateService: NavigateService
   ) {}
 
   navigateToDashboard(dashboardId: string) {
     this.navigateService.navigateToDashboard({ dashboardId: dashboardId });
-  }
-
-  rowMenuOnClick(event: any) {
-    event.stopPropagation();
-  }
-
-  goToDashboardFile(event: any, dashboard: common.DashboardX) {
-    event.stopPropagation();
-
-    let fileIdAr = dashboard.filePath.split('/');
-    fileIdAr.shift();
-
-    this.navigateService.navigateToFileLine({
-      panel: common.PanelEnum.Tree,
-      underscoreFileId: fileIdAr.join(common.TRIPLE_UNDERSCORE)
-    });
-  }
-
-  deleteDashboard(event: MouseEvent, item: common.DashboardX) {
-    event.stopPropagation();
-
-    this.myDialogService.showDeleteDashboard({
-      dashboard: item,
-      apiService: this.apiService,
-      projectId: this.nav.projectId,
-      branchId: this.nav.branchId,
-      envId: this.nav.envId,
-      isRepoProd: this.nav.isRepoProd,
-      isStartSpinnerUntilNavEnd: false
-    });
   }
 
   trackByFn(index: number, item: common.DashboardX) {
