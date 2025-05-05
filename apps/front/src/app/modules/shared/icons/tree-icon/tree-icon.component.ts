39bbab2ee46aa7c64736ee89a7114f545649a97f
akalitenya <akalitenya@mprove.io>
2025-05-05 12:58:11 +0000

    m1572

--- apps/front/src/app/modules/shared/icons/tree-icon/tree-icon.component.ts
+++ apps/front/src/app/modules/shared/icons/tree-icon/tree-icon.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'm-tree-icon',
   templateUrl: 'tree-icon.component.html'
 })
 export class TreeIconComponent {
+  @Input()
+  isFlat: boolean;
+
   constructor() {}
 }
