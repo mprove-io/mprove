5560c433310865bb0abfcc47deb4722795bd86ec
akalitenya <akalitenya@mprove.io>
2025-05-11 13:11:13 +0000

    m1618

--- apps/front/src/app/modules/shared/icons/filter-icon/filter-icon.component.ts
+++ apps/front/src/app/modules/shared/icons/filter-icon/filter-icon.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'm-filter-icon',
   templateUrl: 'filter-icon.component.html'
 })
 export class FilterIconComponent {
+  @Input()
+  isFilled: boolean;
+
   constructor() {}
 }
