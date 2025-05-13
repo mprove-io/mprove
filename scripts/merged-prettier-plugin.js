69b798a7fe5ce47d2905734cb94227409e66bbc5
akalitenya <akalitenya@mprove.io>
2025-05-13 13:04:59 +0000

    m1663

--- scripts/merged-prettier-plugin.js
+++ scripts/merged-prettier-plugin.js
@@ -1,20 +1,20 @@
 const tailwind = require('prettier-plugin-tailwindcss');
-const organizeImports = require('prettier-plugin-organize-imports');
+// const organizeImports = require('prettier-plugin-organize-imports');
 
 const combinedFormatter = {
-  ...tailwind,
-  parsers: {
-    ...tailwind.parsers,
-    ...Object.keys(organizeImports.parsers).reduce((acc, key) => {
-      acc[key] = {
-        ...tailwind.parsers[key],
-        preprocess(code, options) {
-          return organizeImports.parsers[key].preprocess(code, options);
-        }
-      };
-      return acc;
-    }, {})
-  }
+  ...tailwind
+  // parsers: {
+  //   ...tailwind.parsers,
+  //   ...Object.keys(organizeImports.parsers).reduce((acc, key) => {
+  //     acc[key] = {
+  //       ...tailwind.parsers[key],
+  //       preprocess(code, options) {
+  //         return organizeImports.parsers[key].preprocess(code, options);
+  //       }
+  //     };
+  //     return acc;
+  //   }, {})
+  // }
 };
 
 module.exports = combinedFormatter;
