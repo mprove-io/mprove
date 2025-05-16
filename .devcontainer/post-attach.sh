a6c42e62ad0e64085e22f653b24162333efd2059
akalitenya <akalitenya@mprove.io>
2025-05-16 21:42:03 +0000

    m1698

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -3,7 +3,7 @@
 echo "post-attach ...";
 
 cp -n /mprove/.devcontainer/.zshrc.example /mprove/.devcontainer/.zshrc 
-ln -sf /mprove/.devcontainer/.zshrc /root/.zshrc 
+cp /mprove/.devcontainer/.zshrc /root/.zshrc 
 
 # instead of post-create.sh ${containerWorkspaceFolder}
 # echo $1;
