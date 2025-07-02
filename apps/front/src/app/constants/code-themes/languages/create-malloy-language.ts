6dce0dd43c00290639e9953765b2e675488bc637
akalitenya <akalitenya@mprove.io>
2025-07-02 12:19:31 +0000

    m1890

--- apps/front/src/app/constants/code-themes/languages/create-malloy-language.ts
+++ apps/front/src/app/constants/code-themes/languages/create-malloy-language.ts
@@ -1,7 +1,14 @@
 import { StreamLanguage } from '@codemirror/language';
 import { OnigScanner, OnigString, loadWASM } from 'vscode-oniguruma';
-import { INITIAL, Registry, parseRawGrammar } from 'vscode-textmate';
+import {
+  INITIAL,
+  Registry,
+  StateStack,
+  parseRawGrammar
+} from 'vscode-textmate';
 import { MALLOY_GRAMMAR } from '../grammars/malloy-grammar';
+import { SQL_GRAMMAR } from '../grammars/sql-grammar';
+import { sqlScopeToStyle } from './create-sql-language';
 
 // Map TextMate scopes to CodeMirror theme styles
 const malloyScopeToStyle = {
@@ -54,39 +61,88 @@ export async function createMalloyLanguage() {
     loadGrammar: async scopeName => {
       if (
         scopeName === 'source.malloy' ||
-        scopeName === 'source.sql' ||
         scopeName === 'source.malloy-in-sql'
       ) {
-        let malloyRawGrammar = JSON.stringify(MALLOY_GRAMMAR);
-        return parseRawGrammar(malloyRawGrammar, 'sql.tmGrammar.json');
+        return parseRawGrammar(
+          JSON.stringify(MALLOY_GRAMMAR),
+          'malloy.tmGrammar.json'
+        );
+      } else if (scopeName === 'source.sql') {
+        const SQL_GRAMMAR_EXTENDED = Object.assign({}, SQL_GRAMMAR, {
+          patterns: [
+            {
+              name: 'punctuation.sql-block.close',
+              match: '"""',
+              captures: { '0': { name: 'punctuation.sql-block.close' } }
+            },
+            {
+              name: 'source.malloy-in-sql',
+              begin: '%{',
+              end: '}%?',
+              beginCaptures: {
+                '0': { name: 'punctuation.malloy-in-sql.begin' }
+              },
+              endCaptures: { '0': { name: 'punctuation.malloy-in-sql.end' } },
+              patterns: [{ include: 'source.malloy' }]
+            },
+            ...SQL_GRAMMAR.patterns
+          ]
+        });
+
+        return parseRawGrammar(
+          JSON.stringify(SQL_GRAMMAR_EXTENDED),
+          'sql.tmGrammar.json'
+        );
       }
+
       console.log(`Unknown scope name: ${scopeName}`);
       return null;
     }
   });
 
-  const grammar = await registry.loadGrammar('source.malloy');
+  const malloyGrammar = await registry.loadGrammar('source.malloy');
+  const sqlGrammar = await registry.loadGrammar('source.sql');
 
-  if (!grammar) throw new Error('Failed to load Malloy grammar');
+  if (!malloyGrammar) throw new Error('Failed to load Malloy grammar');
+  if (!sqlGrammar) throw new Error('Failed to load SQL grammar');
 
   // console.log('grammar');
   // console.log(grammar);
 
   let malloyStreamParser = {
-    startState: () => ({}),
+    startState: () => ({
+      ruleStack: { malloy: INITIAL, sql: INITIAL },
+      currentGrammar: 'malloy' // Track current grammar (malloy or sql)
+    }),
 
-    token(stream: any, state: any): any {
+    token(
+      stream: any,
+      state: any
+      // {
+      //   malloyRuleStack: StateStack;
+      //   sqlRuleStack: StateStack;
+      //   currentGrammar: string; // malloy or sql
+      // }
+    ): any {
       if (stream.eol()) {
         return null;
       }
 
       let line = stream.string;
 
-      let ruleStack = state.ruleStack || INITIAL;
+      // console.log('state.currentGrammar');
+      // console.log(state.currentGrammar);
+
+      let grammar = state.currentGrammar === 'sql' ? sqlGrammar : malloyGrammar;
+
+      let ruleStack: StateStack =
+        (state.currentGrammar === 'sql'
+          ? state.sqlRuleStack
+          : state.malloyRuleStack) || INITIAL;
 
       let tokenizeResult = grammar.tokenizeLine(line, ruleStack);
 
-      state.ruleStack = tokenizeResult.ruleStack;
+      ruleStack = tokenizeResult.ruleStack;
 
       let currentPos = stream.pos;
 
@@ -106,7 +162,26 @@ export async function createMalloyLanguage() {
 
       let scope = token.scopes[token.scopes.length - 1];
 
-      for (let [textMateScope, cmStyle] of Object.entries(malloyScopeToStyle)) {
+      if (scope === 'source.sql' || scope === 'punctuation.malloy-in-sql.end') {
+        state.currentGrammar = 'sql';
+      } else if (
+        scope === 'source.malloy-in-sql' ||
+        scope === 'punctuation.sql-block.close'
+      ) {
+        state.currentGrammar = 'malloy';
+      }
+
+      let sqlScopeToStyleExtended = {
+        ...sqlScopeToStyle,
+        'punctuation.sql-block.close': 'punctuation',
+        'source.malloy-in-sql': 'malloy-in-sql',
+        'punctuation.malloy-in-sql.begin': 'punctuation',
+        'punctuation.malloy-in-sql.end': 'punctuation'
+      };
+
+      for (let [textMateScope, cmStyle] of Object.entries(
+        scope === 'source.sql' ? sqlScopeToStyleExtended : malloyScopeToStyle
+      )) {
         if (scope.includes(textMateScope)) {
           return cmStyle;
         }
