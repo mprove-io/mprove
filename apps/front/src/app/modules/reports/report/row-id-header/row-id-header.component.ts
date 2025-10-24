c25bd4afa39b07cecb6b9d22f9dfe74db6164f0f
akalitenya <akalitenya@mprove.io>
2025-10-24 11:03:32 +0000

    m2540

--- apps/front/src/app/modules/reports/report/row-id-header/row-id-header.component.ts
+++ apps/front/src/app/modules/reports/report/row-id-header/row-id-header.component.ts
@@ -1,6 +1,10 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { IHeaderAngularComp } from 'ag-grid-angular';
 import { IHeaderParams } from 'ag-grid-community';
+import { tap } from 'rxjs';
+import { RESTRICTED_USER_ALIAS } from '~common/constants/top';
+import { MemberQuery } from '~front/app/queries/member.query';
+import { UserQuery } from '~front/app/queries/user.query';
 import { ApiService } from '~front/app/services/api.service';
 import { MyDialogService } from '~front/app/services/my-dialog.service';
 
@@ -12,8 +16,28 @@ import { MyDialogService } from '~front/app/services/my-dialog.service';
 export class RowIdHeaderComponent implements IHeaderAngularComp {
   params: IHeaderParams;
 
+  restrictedUserAlias = RESTRICTED_USER_ALIAS;
+
+  isExplorer = false;
+  isExplorer$ = this.memberQuery.isExplorer$.pipe(
+    tap(x => {
+      this.isExplorer = x;
+      this.cd.detectChanges();
+    })
+  );
+
+  alias: string;
+  alias$ = this.userQuery.alias$.pipe(
+    tap(x => {
+      this.alias = x;
+      this.cd.detectChanges();
+    })
+  );
+
   constructor(
     private myDialogService: MyDialogService,
+    private memberQuery: MemberQuery,
+    private userQuery: UserQuery,
     private apiService: ApiService,
     private cd: ChangeDetectorRef
   ) {}
