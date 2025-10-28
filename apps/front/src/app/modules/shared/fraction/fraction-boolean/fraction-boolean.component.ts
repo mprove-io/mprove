13c3de4fcde2e4a77f9e970b7f8735860b4f2344
akalitenya <akalitenya@mprove.io>
2025-10-28 04:28:24 +0000

    m2577

--- apps/front/src/app/modules/shared/fraction/fraction-boolean/fraction-boolean.component.ts
+++ apps/front/src/app/modules/shared/fraction/fraction-boolean/fraction-boolean.component.ts
@@ -64,7 +64,7 @@ export class FractionBooleanComponent {
     },
     {
       operator: FractionOperatorEnum.And,
-      label: 'is false',
+      label: 'is false (not null)',
       value: FractionTypeEnum.BooleanIsFalse
     },
     {
