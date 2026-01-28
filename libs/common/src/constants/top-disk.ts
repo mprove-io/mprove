0132c3c74bce373351280be4034a6564359c8a91
akalitenya <akalitenya@mprove.io>
2026-01-28 11:43:24 +0000

    s22

--- libs/common/src/constants/top-disk.ts
+++ libs/common/src/constants/top-disk.ts
@@ -2,4 +2,4 @@ export const APP_NAME_DISK = 'DISK';
 
 export const CENTRAL_REPO_ID = 'central';
 
-export const TEST_PROJECTS = 'apps/disk/src/assets/test-projects';
+export const TEST_PROJECTS = 'src/assets/test-projects';
