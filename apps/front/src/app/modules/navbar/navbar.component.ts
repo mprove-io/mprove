01a3508e40872e803ee2ed8fd50b5df75edb5ac1
akalitenya <akalitenya@mprove.io>
2026-02-21 03:46:01 +0000

    s291

--- apps/front/src/app/modules/navbar/navbar.component.ts
+++ apps/front/src/app/modules/navbar/navbar.component.ts
@@ -4,7 +4,6 @@ import { filter, tap } from 'rxjs/operators';
 import {
   LAST_SELECTED_CHART_ID,
   LAST_SELECTED_DASHBOARD_ID,
-  LAST_SELECTED_FILE_ID,
   LAST_SELECTED_MODEL_ID,
   LAST_SELECTED_REPORT_ID,
   PATH_BUILDER,
@@ -12,6 +11,9 @@ import {
   PATH_MODELS,
   PATH_REPORTS
 } from '#common/constants/top';
+import { PanelEnum } from '#common/enums/panel.enum';
+import { getFileIds } from '#common/functions/get-file-ids';
+import { isDefined } from '#common/functions/is-defined';
 import { Member } from '#common/interfaces/backend/member';
 import { MemberQuery } from '#front/app/queries/member.query';
 import { NavQuery, NavState } from '#front/app/queries/nav.query';
@@ -20,6 +22,7 @@ import { StructQuery, StructState } from '#front/app/queries/struct.query';
 import { UiQuery } from '#front/app/queries/ui.query';
 import { UserQuery } from '#front/app/queries/user.query';
 import { NavigateService } from '#front/app/services/navigate.service';
+import { UiService } from '#front/app/services/ui.service';
 
 @Component({
   standalone: false,
@@ -83,7 +86,8 @@ export class NavbarComponent implements OnInit {
     private uiQuery: UiQuery,
     private userQuery: UserQuery,
     private memberQuery: MemberQuery,
-    private cd: ChangeDetectorRef
+    private cd: ChangeDetectorRef,
+    private uiService: UiService
   ) {}
 
   ngOnInit() {
@@ -111,9 +115,28 @@ export class NavbarComponent implements OnInit {
       return;
     }
 
-    this.navigateService.navigateToFileLine({
-      encodedFileId: LAST_SELECTED_FILE_ID
-    });
+    let repo = this.repoQuery.getValue();
+    let fileIds = getFileIds({ nodes: repo.nodes });
+    let projectFileLinks = this.uiQuery.getValue().projectFileLinks;
+
+    let pLink = projectFileLinks.find(
+      link => link.projectId === this.nav.projectId
+    );
+
+    if (isDefined(pLink)) {
+      let pFileId = fileIds.find(fileId => fileId === pLink.fileId);
+
+      if (isDefined(pFileId)) {
+        this.uiService.ensureFilesLeftPanel();
+        this.navigateService.navigateToFileLine({
+          panel: PanelEnum.Tree,
+          encodedFileId: pFileId
+        });
+        return;
+      }
+    } else {
+      this.navigateService.navigateToBuilder();
+    }
   }
 
   navigateCharts() {
