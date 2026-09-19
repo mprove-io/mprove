235158b7abc9edb8e8a0a6e69b24a00ade3ad245
akalitenya <akalitenya@mprove.io>
2026-09-19 14:37:03 +0000

    k153

--- apps/disk/src/controllers/01-orgs/create-org/create-org.service.ts
+++ apps/disk/src/controllers/01-orgs/create-org/create-org.service.ts
@@ -5,7 +5,7 @@ import type { ToDiskResponseResultForOperation } from '#common/zod/disk/response
 import type { ToDiskCreateOrgOutput } from '#common/zod/disk/routes/01-orgs/create-org/create-org-response';
 import type { DiskConfig } from '#disk/config/disk-config';
 import { ensureDir } from '#disk/functions/disk/ensure-dir';
-import { checkOrgDoesNotExist } from './check-org-does-not-exist';
+import { checkOrgDoesNotExist } from './check-org-does-not-exist/check-org-does-not-exist';
 
 @Injectable()
 export class CreateOrgService {
